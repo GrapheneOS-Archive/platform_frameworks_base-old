c8e9ecf660da86c3a3cb93aaeeee33f6809c2f3c
Daniel Micay <danielmicay@gmail.com>
2017-06-29 10:16:52 -0400

    increase default maximum password length
    Change-Id: I94b2f8f160920dbf692aa26078d5e40ce6dd1152

--- core/java/android/app/admin/DevicePolicyManager.java
+++ core/java/android/app/admin/DevicePolicyManager.java
@@ -2511,7 +2511,7 @@ public class DevicePolicyManager {
      */
     public int getPasswordMaximumLength(int quality) {
         // Kind-of arbitrary.
-        return 16;
+        return 64;
     }
 
     /**
