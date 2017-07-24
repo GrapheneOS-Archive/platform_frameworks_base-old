acc443885312fc0dae7d5aa445ece23e59b25c29
Daniel Micay <danielmicay@gmail.com>
2017-07-24 12:30:32 -0400

    stop granting location to Chromium by default
    It works fine without it and requests it after the user grants access to
    a site.
    
    Change-Id: Ifabc3f1ae4acf008abf1467fc928eeb90613feff

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -557,7 +557,7 @@ final class DefaultPermissionGrantPolicy {
             }
             if (browserPackage != null
                     && doesPackageSupportRuntimePermissions(browserPackage)) {
-                grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, userId);
+                //grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, userId);
             }
 
             // Voice interaction
@@ -861,7 +861,7 @@ final class DefaultPermissionGrantPolicy {
         PackageParser.Package browserPackage = getSystemPackageLPr(packageName);
         if (browserPackage != null
                 && doesPackageSupportRuntimePermissions(browserPackage)) {
-            grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, false, false, userId);
+            //grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, false, false, userId);
         }
     }
 
