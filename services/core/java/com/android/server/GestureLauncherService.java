be0ecc1b33417d935aab5a70f708efbaa674aedd
Daniel Micay <danielmicay@gmail.com>
2017-07-25 11:22:33 -0400

    add system property for disabling keyguard camera
    Change-Id: I0b65cac3c3d2fc495b339c34add742bd698b107c

--- services/core/java/com/android/server/GestureLauncherService.java
+++ services/core/java/com/android/server/GestureLauncherService.java
@@ -17,6 +17,7 @@
 package com.android.server;
 
 import android.app.ActivityManager;
+import android.app.KeyguardManager;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -436,6 +437,13 @@ public class GestureLauncherService extends SystemService {
     boolean handleCameraGesture(boolean useWakelock, int source) {
         Trace.traceBegin(Trace.TRACE_TAG_ACTIVITY_MANAGER, "GestureLauncher:handleCameraGesture");
         try {
+            if (!SystemProperties.getBoolean("persist.keyguard.camera", true)) {
+                final KeyguardManager manager = (KeyguardManager) getContext().getSystemService(
+                        Context.KEYGUARD_SERVICE);
+                if (manager.inKeyguardRestrictedInputMode()) {
+                    return false;
+                }
+            }
             boolean userSetupComplete = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.USER_SETUP_COMPLETE, 0, UserHandle.USER_CURRENT) != 0;
             if (!userSetupComplete) {
