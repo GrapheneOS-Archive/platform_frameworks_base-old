7c787f07d20028bc0cc35852be60134178c7cd41
Daniel Micay <danielmicay@gmail.com>
2017-10-07 15:54:42 -0400

    add special runtime permission for other sensors
    This covers sensors not included in the existing runtime permission for
    body sensors.

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -660,7 +660,7 @@ public class PermissionManagerService {
     }
 
     public static boolean isSpecialRuntimePermission(final String permission) {
-        return Manifest.permission.INTERNET.equals(permission);
+        return Manifest.permission.INTERNET.equals(permission) || Manifest.permission.OTHER_SENSORS.equals(permission);
     }
 
     private void grantPermissions(PackageParser.Package pkg, boolean replace,
