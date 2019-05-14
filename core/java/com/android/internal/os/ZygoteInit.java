e2fe8e29ee50e67b926e5a2d149adef9c39968d6
Daniel Micay <danielmicay@gmail.com>
2019-05-14 14:24:21 -0400

    add parameter for avoiding full preload with exec

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -133,7 +133,7 @@ public class ZygoteInit {
      */
     private static ClassLoader sCachedSystemServerClassLoader = null;
 
-    static void preload(TimingsTraceLog bootTimingsTraceLog) {
+    static void preload(TimingsTraceLog bootTimingsTraceLog, boolean fullPreload) {
         Log.d(TAG, "begin preload");
         bootTimingsTraceLog.traceBegin("BeginPreload");
         beginPreload();
@@ -163,6 +163,10 @@ public class ZygoteInit {
         Log.d(TAG, "end preload");
 
         sPreloadComplete = true;
+    }
+
+    static void preload(TimingsTraceLog bootTimingsTraceLog) {
+        preload(bootTimingsTraceLog, true);
     }
 
     static void lazyPreload() {
