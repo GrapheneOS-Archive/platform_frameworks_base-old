324d5f8fa08028b30d414e9385eaca7b9b3207d9
Renlord <me@renlord.com>
2020-03-03 22:55:39 +1100

    use per-user settings secure table for lockscreen pin scrambling layout option
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Change-Id: I484405a4f2f30d23f4143597cb944e515ce6eb43

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4179,13 +4179,6 @@ public final class Settings {
             "lock_pattern_tactile_feedback_enabled";
 
         /**
-         * Whether to scramble a pin unlock layout
-         * @hide
-         */
-        public static final String SCRAMBLE_PIN_LAYOUT =
-            "lockscreen_scramble_pin_layout";
-
-        /**
          * A formatted string of the next alarm that is set, or the empty string
          * if there is no alarm set.
          *
@@ -9893,6 +9886,13 @@ public final class Settings {
          */
         @Readable
         public static final String PEOPLE_STRIP = "people_strip";
+
+        /**
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String SCRAMBLE_PIN_LAYOUT =
+            "lockscreen_scramble_pin_layout";
 
         /**
          * Whether or not to enable media resumption
