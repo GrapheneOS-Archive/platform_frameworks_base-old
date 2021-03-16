7cb90f7424d4aa222dc3a6faa502a377cfe107a5
Daniel Micay <danielmicay@gmail.com>
2021-03-16 02:54:57 -0400

    enable app compaction by default

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -90,7 +90,7 @@ public final class CachedAppOptimizer {
     private static final String COMPACT_ACTION_FULL = "all";
 
     // Defaults for phenotype flags.
-    @VisibleForTesting static final Boolean DEFAULT_USE_COMPACTION = false;
+    @VisibleForTesting static final Boolean DEFAULT_USE_COMPACTION = true;
     @VisibleForTesting static final Boolean DEFAULT_USE_FREEZER = false;
     @VisibleForTesting static final int DEFAULT_COMPACT_ACTION_1 = COMPACT_ACTION_FILE_FLAG;
     @VisibleForTesting static final int DEFAULT_COMPACT_ACTION_2 = COMPACT_ACTION_FULL_FLAG;
