e76749375ca1952a41cf8c9bc2196a0a95618cc0
Danny Lin <danny@kdrag0n.dev>
2021-07-08 03:09:36 -0700

    gmscompat: Keep GMS services alive by converting to foreground services
    Unprivileged apps can't be exempt from the background execution
    requirements, so keep GMS services alive by converting them to
    foreground services.
    
    Apps must post a foreground service notification for each service within
    10 seconds of the service starting or the system will kill the entire
    process, so generate notifications dynamically from fully-qualified
    service class names and post them automatically for GMS apps.
    
    Change-Id: I0c8a44bf86c364dbbd22ef18138a9a57c86a7bdc

--- core/java/com/android/internal/gmscompat/GmsHooks.java
+++ core/java/com/android/internal/gmscompat/GmsHooks.java
@@ -16,6 +16,17 @@
 
 package com.android.internal.gmscompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.compat.gms.GmsCompat;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * API shims for Google Play Services compatibility. Hooks that are more complicated than a simple
  * constant return value should be delegated to this class for easier maintenance.
@@ -25,6 +36,72 @@ package com.android.internal.gmscompat;
 public final class GmsHooks {
     private static final String TAG = "GmsCompat/Hooks";
 
+    // Foreground service notifications
+    private static final String FGS_CHANNEL_ID = "service_shim";
+    private static boolean fgsChannelCreated = false;
+
     // Static only
     private GmsHooks() { }
+
+    /*
+     * Foreground service notifications to keep GMS services alive
+     */
+
+    // Make all services foreground to keep them alive
+    // ContextImpl#startService(Intent)
+    public static ComponentName startService(Context context, Intent service) {
+        return context.startForegroundService(service);
+    }
+
+    private static void createFgsChannel(Service service) {
+        if (fgsChannelCreated) {
+            return;
+        }
+
+        NotificationManager notificationManager = (NotificationManager)
+                service.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        CharSequence name = service.getText(
+                com.android.internal.R.string.foreground_service_gms_shim_category);
+        NotificationChannel channel = new NotificationChannel(FGS_CHANNEL_ID, name,
+                NotificationManager.IMPORTANCE_LOW);
+        notificationManager.createNotificationChannel(channel);
+
+        fgsChannelCreated = true;
+    }
+
+    // Post notification on foreground service start
+    // ActivityThread#handleCreateService(CreateServiceData)
+    public static void attachService(Service service) {
+        if (!GmsCompat.isEnabled()) {
+            return;
+        }
+
+        // Shorten name for display
+        String serviceName = service.getClass().getName()
+                .replace("com.google.android.gms.", "c.g.a.g.")
+                .replace("com.google.android.", "c.g.a.")
+                .replace("com.google.", "c.g.");
+
+        // Channel
+        createFgsChannel(service);
+        // Notification
+        PendingIntent pi = PendingIntent.getActivity(service, 100, new Intent(),
+                PendingIntent.FLAG_IMMUTABLE);
+        Notification notification = new Notification.Builder(service, FGS_CHANNEL_ID)
+                .setSmallIcon(service.getApplicationInfo().icon)
+                .setContentTitle(serviceName)
+                .setContentIntent(pi)
+                .build();
+
+        Log.d(TAG, "Posting notification for service: " + service.getClass().getName());
+        service.startForeground(serviceName.hashCode(), notification);
+    }
+
+    // GMS tries to clean up its own notification channels periodically.
+    // Don't let it delete the FGS shim channel because that throws an exception and crashes GMS.
+    // NotificationManager#deleteNotificationChannel(String)
+    public static boolean skipDeleteNotificationChannel(String channelId) {
+        return GmsCompat.isEnabled() && FGS_CHANNEL_ID.equals(channelId);
+    }
 }
