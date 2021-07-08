b4e9fa1f0283ee70f536d139b0cef0883d32c5f8
Danny Lin <danny@kdrag0n.dev>
2021-07-07 21:39:20 -0700

    gmscompat: Add helpers for Google Play Services compatibility
    This adds a set of helper classes and methods for our upcoming Google
    Play Services compatibility, which will make it possible to run Google
    Play Services (aka. Google Mobile Services / "GMS") and related apps
    (Google Services Framework, Google Play Store) as regular, unprivileged
    user apps.
    
    Implementation notes:
      - The core GmsCompat class is in android.app.gmscompat and exposes
        isEnabled() to system modules because WifiManager, which is part of
        the com.android.wifi APEX module, will need to use it.
      - GmsCompat should only be used for core functionality. API shim
        implementations belong in GmsHooks.
      - The flag has been implemented as a compatibility change,
        GMS_UNPRIVILEGED_COMPAT, in order to reuse the existing
        compatibility change infrastructure.
    
    The actual API shims and compatibility will be added in following
    commits.
    
    Change-Id: Ibfabb6465c40280d4ce73c120ff1f18230fbe2d3

---
+++ core/java/android/app/compat/gms/GmsCompat.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app.compat.gms;
+
+import android.annotation.SystemApi;
+import android.app.ActivityThread;
+import android.compat.Compatibility;
+import android.compat.annotation.ChangeId;
+import android.compat.annotation.Disabled;
+import android.content.pm.Signature;
+import android.os.Process;
+import android.util.Log;
+
+import com.android.internal.gmscompat.GmsInfo;
+
+/**
+ * This class provides helpers for Google Play Services compatibility. It allows the following apps
+ * to work as regular, unprivileged user apps:
+ *     - Google Play Services (Google Mobile Services, aka "GMS")
+ *     - Google Services Framework
+ *     - Google Play Store
+ *     - All apps depending on Google Play Services
+ *
+ * All GMS compatibility hooks should call methods on GmsCompat. Hooks that are more complicated
+ * than returning a simple constant value should also be implemented in GmsHooks to reduce
+ * maintenance overhead.
+ *
+ * @hide
+ */
+@SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+public final class GmsCompat {
+    private static final String TAG = "GmsCompat/Core";
+
+    /**
+     * Whether to enable Google Play Services compatibility for this app.
+     *
+     * This compatibility change is special because the system enables it automatically for certain
+     * apps, but it still needs to be declared with a change ID.
+     *
+     * We don't have a bug for this in Google's issue tracker, so the change ID is a
+     * randomly-generated long.
+     */
+    @ChangeId
+    @Disabled
+    private static final long GMS_UNPRIVILEGED_COMPAT = 1531297613045645771L;
+
+    private static final boolean DEBUG_VERBOSE = false;
+
+    // Static only
+    private GmsCompat() { }
+
+    private static void logEnabled(boolean enabled) {
+        if (!DEBUG_VERBOSE) {
+            return;
+        }
+
+        String pkg = ActivityThread.currentPackageName();
+        if (pkg == null) {
+            pkg = (Process.myUid() == Process.SYSTEM_UID) ? "system_server" : "[unknown]";
+        }
+
+        Log.d(TAG, "Enabled for " + pkg + " (" + Process.myPid() + "): " + enabled);
+    }
+
+    public static boolean isEnabled() {
+        boolean enabled = Compatibility.isChangeEnabled(GMS_UNPRIVILEGED_COMPAT);
+
+        // Compatibility changes aren't available in the system process, but this should never be
+        // enabled for it.
+        if (Process.myUid() == Process.SYSTEM_UID) {
+            enabled = false;
+        }
+
+        logEnabled(enabled);
+        return enabled;
+    }
+
+    /**
+     * Check whether the given app is part of the Google Play Services family.
+     *
+     * @hide
+     */
+    public static boolean isGmsApp(String packageName, Signature[] signatures) {
+        if (!GmsInfo.PACKAGE_GMS.equals(packageName) &&
+                !GmsInfo.PACKAGE_GSF.equals(packageName) &&
+                !GmsInfo.PACKAGE_PLAY_STORE.equals(packageName)) {
+            return false;
+        }
+
+        // Validate signature to avoid affecting apps like microG and Gcam Services Provider.
+        // This isn't actually necessary from a security perspective because GMS doesn't get any
+        // special privileges.
+        boolean validCert = false;
+        for (Signature signature : signatures) {
+            if (signature.toCharsString().equals(GmsInfo.SIGNING_CERT)) {
+                validCert = true;
+            }
+        }
+
+        return validCert;
+    }
+}
