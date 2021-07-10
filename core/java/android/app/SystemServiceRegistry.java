727a521670cf751ec7486256176fb2a0f646f5e0
Danny Lin <danny@kdrag0n.dev>
2021-07-10 02:11:04 -0700

    gmscompat: Provide a dummy implementation of AppIntegrityManager
    Newer versions of GMS crash if this service isn't available:
    
    FATAL EXCEPTION: [com.google.android.gms.chimera.GmsIntentOperationService$ChimeraService-Executor] idle
    Process: com.google.android.gms, PID: 23019
    java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.String android.content.integrity.AppIntegrityManager.getCurrentRuleSetProvider()' on a null object reference
           at com.google.android.gms.appintegrity.service.AppIntegrityModuleInitIntentOperation.e(:com.google.android.gms@212415046@21.24.15 (150400-381273646):13)
           at rpe.onHandleIntent(:com.google.android.gms@212415046@21.24.15 (150400-381273646):8)
           at com.google.android.chimera.IntentOperation.onHandleIntent(:com.google.android.gms@212415046@21.24.15 (150400-381273646):2)
           at roy.onHandleIntent(:com.google.android.gms@212415046@21.24.15 (150400-381273646):4)
           at eck.run(:com.google.android.gms@212415046@21.24.15 (150400-381273646):5)
           at ecj.run(:com.google.android.gms@212415046@21.24.15 (150400-381273646):11)
           at bvgf.run(:com.google.android.gms@212415046@21.24.15 (150400-381273646):2)
           at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
           at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
           at java.lang.Thread.run(Thread.java:923)
    
    Change-Id: Ic43702b78f7986d7739218fb6d0df2a26f7df270

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -25,6 +25,7 @@ import android.app.ContextImpl.ServiceInitializationState;
 import android.app.admin.DevicePolicyManager;
 import android.app.admin.IDevicePolicyManager;
 import android.app.blob.BlobStoreManagerFrameworkInitializer;
+import android.app.compat.gms.GmsCompat;
 import android.app.contentsuggestions.ContentSuggestionsManager;
 import android.app.contentsuggestions.IContentSuggestionsManager;
 import android.app.job.JobSchedulerFrameworkInitializer;
@@ -1323,6 +1324,10 @@ public final class SystemServiceRegistry {
                     @Override
                     public AppIntegrityManager createService(ContextImpl ctx)
                             throws ServiceNotFoundException {
+                        if (GmsCompat.isEnabled()) {
+                            return new AppIntegrityManager(null);
+                        }
+
                         IBinder b = ServiceManager.getServiceOrThrow(Context.APP_INTEGRITY_SERVICE);
                         return new AppIntegrityManager(IAppIntegrityManager.Stub.asInterface(b));
                     }});
