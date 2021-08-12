636d774a6124bfcb2b8e8bb7ec233ec2744ed4f7
Daniel Micay <danielmicay@gmail.com>
2021-08-12 17:23:13 -0400

    Revert "Display UART notification on user builds"
    This reverts commit 548b719826263226304b9be097f238d7fb07c02d.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -386,7 +386,6 @@ import libcore.util.EmptyArray;
 
 import java.io.File;
 import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -407,7 +406,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
@@ -5571,28 +5569,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         t.traceEnd();
     }
 
-    private static Boolean isUartEnabled() {
-        // console=null should be set in the kernel cmdline when UART is off
-        final String console_string = "console=null";
-        boolean isEnabled = false;
-        try (Scanner sc = new Scanner(new FileInputStream("/proc/cmdline"))) {
-            StringBuilder scOutput = new StringBuilder();
-            while(sc.hasNextLine()){
-                scOutput.append(sc.nextLine());
-            }
-            isEnabled = !scOutput.toString().contains(console_string);
-        } catch (IOException ignored) {
-        }
-        // This check will only work on userdebug and eng builds due
-        // to the console service not being present on user builds.
-        if (SystemProperties.get("init.svc.console").equals("running")) {
-            isEnabled = true;
-        }
-        return isEnabled;
-    }
-
     private void showConsoleNotificationIfActive() {
-        if (!isUartEnabled()) {
+        if (!SystemProperties.get("init.svc.console").equals("running")) {
             return;
         }
         String title = mContext
