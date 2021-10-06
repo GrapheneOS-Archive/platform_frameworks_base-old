5d0a7ae8c8df41e193f02091aa615a8a0edffbd7
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:01:09 -0700

    SystemUI: Require unlocking to use sensitive QS tiles
    - Airplane mode
    - Battery saver
    - Bluetooth
    - Cellular
    - DataSaver
    - Hotspot
    - NFC
    - Rotation lock
    - Wi-Fi
    - Work mode
    
    This is the same idea as the following commits from GrapheneOS, but
    simplified to reduce changes made to each individual QS tile:
    
    commit 45670099ed63c9fbde6ff1bd13d27b674416d21d
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:22:41 2020 -0700
    
        require unlocking to use work mode tile
    
    commit 4b7c9559dfba84e10142fcd53b3edac9c75b0748
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:21:16 2020 -0700
    
        require unlocking to use battery saver tile
    
    commit f45c24a4728531441c60320eda0a4fce4285de06
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:19:36 2020 -0700
    
        require unlocking to use cellular quick tile
    
    commit c18f6f868aee5b8f27fcc1966089f814f9f89ab2
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:15:46 2020 -0700
    
        require unlocking to use hotspot quick tile
    
    commit 442c94e1ee499eed467863362f8eb62bb88027a4
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:13:58 2020 -0700
    
        require unlocking to use data saver quick tile
    
    commit cf84259708f9bf650c8494e16025725b1bfa5e23
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:11:58 2020 -0700
    
        require unlocking to use rotation lock quick tile
    
    commit 61eb5a6392c53e4d44cb91cdb4df27c3055ef439
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:08:33 2020 -0700
    
        require unlocking to use Wi-Fi quick tile
    
    commit ad122f0a5a46e2445b5866d3ff83fdc7623b76f6
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:05:18 2020 -0700
    
        require unlocking to use airplane mode quick tile
    
    commit a3686b6f905fb734b3a4f6db545e8a3ef3fc3cc7
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:02:20 2020 -0700
    
        require unlocking to use Bluetooth quick tile
    
    commit 3046535a40f53835eb0b57d4c12a29828ba90f30
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 16:47:59 2020 -0700
    
        require unlocking to use NFC quick tile
    
    Change-Id: I515f4bdd30f84a4a76f4807afcf3cc5a3ce42fa9

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AirplaneModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AirplaneModeTile.java
@@ -48,13 +48,14 @@ import com.android.systemui.qs.GlobalSetting;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import javax.inject.Inject;
 
 import dagger.Lazy;
 
 /** Quick settings tile: Airplane mode **/
-public class AirplaneModeTile extends QSTileImpl<BooleanState> {
+public class AirplaneModeTile extends SecureQSTile<BooleanState> {
     private final Icon mIcon = ResourceIcon.get(com.android.internal.R.drawable.ic_qs_airplane);
     private final GlobalSetting mSetting;
     private final BroadcastDispatcher mBroadcastDispatcher;
@@ -73,10 +74,11 @@ public class AirplaneModeTile extends QSTileImpl<BooleanState> {
             ActivityStarter activityStarter,
             QSLogger qsLogger,
             BroadcastDispatcher broadcastDispatcher,
-            Lazy<ConnectivityManager> lazyConnectivityManager
+            Lazy<ConnectivityManager> lazyConnectivityManager,
+            KeyguardStateController keyguardStateController
     ) {
         super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
-                statusBarStateController, activityStarter, qsLogger);
+                statusBarStateController, activityStarter, qsLogger, keyguardStateController);
         mBroadcastDispatcher = broadcastDispatcher;
         mLazyConnectivityManager = lazyConnectivityManager;
 
@@ -95,7 +97,11 @@ public class AirplaneModeTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleClick(@Nullable View view) {
+    protected void handleClick(@Nullable View view, boolean keyguardShowing) {
+        if (checkKeyguard(view, keyguardShowing)) {
+            return;
+        }
+
         boolean airplaneModeEnabled = mState.value;
         MetricsLogger.action(mContext, getMetricsCategory(), !airplaneModeEnabled);
         if (!airplaneModeEnabled && TelephonyProperties.in_ecm_mode().orElse(false)) {
