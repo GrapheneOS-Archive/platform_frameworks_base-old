7f98b5f6ab951c5566e6ca09e7d3ea71405bebcb
flawedworld <flawedworld@flawed.world>
2021-10-15 17:07:13 +0100

    enable secondary user logout support by default
    Ported from 11, 12 moved the isLogoutEnabled boolean to ActiveAdmin.java

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -16069,11 +16069,11 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
     @Override
     public boolean isLogoutEnabled() {
         if (!mHasFeature) {
-            return false;
+            return true;
         }
         synchronized (getLockObject()) {
             ActiveAdmin deviceOwner = getDeviceOwnerAdminLocked();
-            return (deviceOwner != null) && deviceOwner.isLogoutEnabled;
+            return (deviceOwner == null) || deviceOwner.isLogoutEnabled;
         }
     }
 
