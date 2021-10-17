1469d2b79f96eecdebfd116032d9a8d10bbc46e9
anupritaisno1 <www.anuprita804@gmail.com>
2021-10-18 04:05:40 +0530

    automatically reboot device after timeout if set
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Change-Id: If891bfbcc144c9336ba013260bad2b7c7a59c054

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16334,6 +16334,13 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
+
+        /**
+         * Whether to automatically reboot the device after a user defined timeout
+         *
+         * @hide
+         */
+        public static final String SETTINGS_REBOOT_AFTER_TIMEOUT = "settings_reboot_after_timeout";
     }
 
     /**
