0f11747125192aaf592a8ba8d5537cf815a755dc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-03-14 20:22:31 +0200

    gmscompat: improve FileProxyService
    - disallow opening of directories
    - restrict file type to ".apk"
    - don't forward exceptions to minimize the information leaks
    - don't check accessibility of "app_chimera/m/" components that
    are known to be accessible
    - optimize the path splitting operations
    - remove the getLastModified() method that is no longer needed

--- core/java/com/android/internal/gmscompat/dynamite/server/FileProxyService.java
+++ core/java/com/android/internal/gmscompat/dynamite/server/FileProxyService.java
@@ -24,77 +24,76 @@ import android.system.OsConstants;
 import android.util.Log;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileDescriptor;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
-/** @hide */
 public final class FileProxyService extends IFileProxyService.Stub {
     public static final String TAG = "GmsCompat/DynamiteServer";
     private static final String CHIMERA_REL_PATH = "app_chimera/m/";
 
-    private final File deDataRoot;
     private final String chimeraRoot;
 
     public FileProxyService(Context context) {
-        deDataRoot = context.createDeviceProtectedStorageContext().getDataDir();
+        File deDataRoot = context.createDeviceProtectedStorageContext().getDataDir();
         chimeraRoot = deDataRoot.getPath() + "/" + CHIMERA_REL_PATH;
-    }
-
-    private String sanitizeModulePath(String rawPath) {
-        // Normalize path for security checks
-        String path;
-        try {
-            path = new File(rawPath).getCanonicalPath();
-        } catch (IOException e) {
-            throw new SecurityException("Invalid path " + rawPath + ": " + e.getMessage());
-        }
-
-        // Modules can only be in DE Chimera storage
-        if (!path.startsWith(chimeraRoot)) {
-            throw new SecurityException("Path " + rawPath + " is not in " + chimeraRoot);
-        }
-
-        // Check permissions recursively
-        String relPath = path.substring(deDataRoot.getPath().length() + 1); // already checked prefix above
-        List<String> relParts = Arrays.asList(relPath.split("/"));
-        for (int i = 0; i < relParts.size(); i++) {
-            List<String> leadingParts = relParts.subList(0, i + 1);
-            String nodePath = deDataRoot + "/" + String.join("/", leadingParts);
-            int mode;
-            try {
-                mode = Os.stat(nodePath).st_mode;
-            } catch (ErrnoException e) {
-                throw new SecurityException("Failed to stat " + rawPath + ": " + e.getMessage());
-            }
-
-            // World-readable or world-executable, depending on type
-            int permBit = OsConstants.S_ISDIR(mode) ? OsConstants.S_IXOTH : OsConstants.S_IROTH;
-            if ((mode & permBit) == 0) {
-                throw new SecurityException("Node " + nodePath + " in path " + rawPath + " is not world-readable");
-            }
-        }
-
-        return path;
-    }
-
-    private ParcelFileDescriptor getFilePfd(String path) {
-        Log.d(TAG, "Opening " + path + " for remote");
-        try {
-            return ParcelFileDescriptor.open(new File(path), ParcelFileDescriptor.MODE_READ_ONLY);
-        } catch (FileNotFoundException e) {
-            return null;
-        }
     }
 
     @Override
     public ParcelFileDescriptor openFile(String rawPath) {
-        return getFilePfd(sanitizeModulePath(rawPath));
+        try {
+            String path = sanitizeModulePath(rawPath);
+            if (path != null) {
+                FileDescriptor fd = Os.open(path, OsConstants.O_RDONLY | OsConstants.O_CLOEXEC, 0);
+//                Log.d(TAG, "Opened " + rawPath + " for remote, fd " + fd.getInt$());
+                return new ParcelFileDescriptor(fd);
+            }
+        } catch (IOException | ErrnoException e) {
+            Log.d(TAG, "failed security check", e);
+        } catch (Throwable t) {
+            Log.d(TAG, "unexpected error", t);
+        }
+        // don't forward exceptions to the untrusted caller to minimize the information leaks
+        return null;
     }
 
-    @Override
-    public long getLastModified(String rawPath) {
-        return new File(sanitizeModulePath(rawPath)).lastModified();
+    private String sanitizeModulePath(String rawPath) throws IOException, ErrnoException {
+        // Normalize path for security checks
+        String path = new File(rawPath).getCanonicalPath();
+
+        // Modules can only be in DE Chimera storage
+        if (!path.startsWith(chimeraRoot)) {
+            Log.d(TAG, "Path " + rawPath + " is not in " + chimeraRoot);
+            return null;
+        }
+
+        if (!path.endsWith(".apk")) {
+            Log.d(TAG, "Path " + rawPath + " is not an APK file");
+            return null;
+        }
+        // Make sure that all path components below chimeraRoot are world-accessible
+        {
+            // Check full path first to simplify checks of its parents
+            int mode = Os.stat(path).st_mode;
+
+            boolean valid = OsConstants.S_ISREG(mode) && (mode & OsConstants.S_IROTH) != 0;
+            if (!valid) {
+                Log.d(TAG, "Path " + path + " is not a world-readable regular file");
+                return null;
+            }
+        }
+        for (int i = chimeraRoot.length(), m = path.length(); i < m; ++i) {
+            if (path.charAt(i) != '/') {
+                continue;
+            }
+            String dirPath = path.substring(0, i);
+            int mode = Os.stat(dirPath).st_mode;
+
+            boolean valid = OsConstants.S_ISDIR(mode) && (mode & OsConstants.S_IXOTH) != 0;
+            if (!valid) {
+                Log.d(TAG, "Node " + dirPath + " in path " + path + " is not a world-readable directory");
+                return null;
+            }
+        }
+        return path;
     }
 }
