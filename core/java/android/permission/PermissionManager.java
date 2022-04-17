b0b483e457e897929c9d7e9d8ae71f31a2a4abaf
Daniel Micay <danielmicay@gmail.com>
2022-04-17 14:45:50 -0400

    add com.android.bluetooth to indicator exemptions
    Scanning for Bluetooth devices when Bluetooth is enabled is considered
    to be location access. It shouldn't be shown for the OS implementation
    of Bluetooth.

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -91,6 +91,7 @@ public final class PermissionManager {
             "permission grant or revoke changed gids";
 
     private static final String SYSTEM_PKG = "android";
+    private static final String BLUETOOTH_PKG = "com.android.bluetooth";
 
     /**
      * Refuse to install package if groups of permissions are bad
@@ -940,6 +941,7 @@ public final class PermissionManager {
         updateIndicatorExemptedPackages(context);
         ArraySet<String> pkgNames = new ArraySet<>();
         pkgNames.add(SYSTEM_PKG);
+        pkgNames.add(BLUETOOTH_PKG);
         for (int i = 0; i < INDICATOR_EXEMPTED_PACKAGES.length; i++) {
             String exemptedPackage = INDICATOR_EXEMPTED_PACKAGES[i];
             if (exemptedPackage != null) {
