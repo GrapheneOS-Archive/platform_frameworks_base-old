641dca0e76e89bb3c3af29c0109b6f37aae5c51d
flawedworld <flawedworld@flawed.world>
2022-06-15 23:42:47 +0300

    Warn the user if they are using a prototype Pixel device
    Consumer Pixel devices have "MP1.0" as their hardware revision usually.
    Devices stolen from Google will have "EVT", "PVT" or "DVT" set in
    "ro.revision" by the bootloader. Additionally, check the secure boot
    state prop set by the bootloader, "ro.boot.secure_boot", to ensure it is
    set to "1", as typically pre-production devices won't have a blown
    secure boot efuse, completely destroying any concept of verified boot.
    Check both props, if either check fails, notify the user.
    
    Squashed with: 778380bcc524954ed1b71dda8c762c458dfec087
    
    Co-authored-by: Dmitry Muhomor <muhomor.dmitry@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5092,6 +5092,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         // UART is on if init's console service is running, send a warning notification.
         showConsoleNotificationIfActive();
         showMteOverrideNotificationIfActive();
+        // If device is not a mass production (MP) variant, send a warning notification.
+        showPrototypeNotificationIfPrototype();
 
         t.traceEnd();
     }
@@ -5113,6 +5115,62 @@ public class ActivityManagerService extends IActivityManager.Stub
             isEnabled = true;
         }
         return isEnabled;
+    }
+
+    private static boolean isPrototypeDevice() {
+        switch (Build.BRAND) {
+            case "google":
+                break;
+            default:
+                // Other OEMs may deal with hardware revisions differently
+                return false;
+        }
+
+        if (!SystemProperties.get("ro.revision").contains("MP")) {
+            // MP1.0 is what all 3rd generation and newer Pixel devices have as their mass
+            // production revision.
+            // Account for future mass production device revisions by only checking for "MP".
+            return true;
+        }
+
+        if (!SystemProperties.get("ro.boot.secure_boot").equals("PRODUCTION")) {
+            // All devices with a blown secure boot fuse will have the bootloader report
+            // ro.boot.secure_boot as being in PRODUCTION mode.
+            // Engineering devices typically don't have a blown secure boot fuse as they are used
+            // for firmware development.
+            return true;
+        }
+
+        return false;
+    }
+
+    private void showPrototypeNotificationIfPrototype() {
+        if (!isPrototypeDevice()) {
+            return;
+        }
+        String title = mContext
+                .getString(com.android.internal.R.string.prototype_device_notification_title);
+        String message = mContext
+                .getString(com.android.internal.R.string.prototype_device_notification_message);
+        Notification notification =
+                new Notification.Builder(mContext, SystemNotificationChannels.DEVELOPER)
+                        .setSmallIcon(com.android.internal.R.drawable.stat_sys_adb)
+                        .setWhen(0)
+                        .setOngoing(true)
+                        .setTicker(title)
+                        .setDefaults(0)  // please be quiet
+                        .setColor(mContext.getColor(
+                                com.android.internal.R.color
+                                        .system_notification_accent_color))
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setVisibility(Notification.VISIBILITY_PUBLIC)
+                        .build();
+
+        NotificationManager notificationManager =
+                mContext.getSystemService(NotificationManager.class);
+        notificationManager.notifyAsUser(
+                null, SystemMessage.NOTE_PROTOTYPE_DETECTED, notification, UserHandle.ALL);
     }
 
     private void showConsoleNotificationIfActive() {
