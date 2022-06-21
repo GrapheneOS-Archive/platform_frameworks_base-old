dd1f259fda819c64077a53fc2edac70ea01124f4
empratyush <codelab@pratyush.dev>
2022-06-21 16:52:04 +0530

    added Battery share & Battery share quick tiles

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -28,6 +28,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
+import com.android.systemui.qs.tiles.BatteryShareTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CameraToggleTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -78,6 +79,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<HotspotTile> mHotspotTileProvider;
     private final Provider<UserTile> mUserTileProvider;
     private final Provider<BatterySaverTile> mBatterySaverTileProvider;
+    private final Provider<BatteryShareTile> mBatteryShareTileProvider;
     private final Provider<DataSaverTile> mDataSaverTileProvider;
     private final Provider<NightDisplayTile> mNightDisplayTileProvider;
     private final Provider<NfcTile> mNfcTileProvider;
@@ -113,6 +115,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<HotspotTile> hotspotTileProvider,
             Provider<UserTile> userTileProvider,
             Provider<BatterySaverTile> batterySaverTileProvider,
+            Provider<BatteryShareTile> batteryShareTileProvider,
             Provider<DataSaverTile> dataSaverTileProvider,
             Provider<NightDisplayTile> nightDisplayTileProvider,
             Provider<NfcTile> nfcTileProvider,
@@ -143,6 +146,7 @@ public class QSFactoryImpl implements QSFactory {
         mHotspotTileProvider = hotspotTileProvider;
         mUserTileProvider = userTileProvider;
         mBatterySaverTileProvider = batterySaverTileProvider;
+        mBatteryShareTileProvider = batteryShareTileProvider;
         mDataSaverTileProvider = dataSaverTileProvider;
         mNightDisplayTileProvider = nightDisplayTileProvider;
         mNfcTileProvider = nfcTileProvider;
@@ -199,6 +203,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mUserTileProvider.get();
             case "battery":
                 return mBatterySaverTileProvider.get();
+            case "batteryShare":
+                return mBatteryShareTileProvider.get();
             case "saver":
                 return mDataSaverTileProvider.get();
             case "night":
