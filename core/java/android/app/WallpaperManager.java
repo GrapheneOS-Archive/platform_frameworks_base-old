eb9b4e0c8ad91c1878293bfbe2878b30371c9759
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-30 18:23:12 +0300

    sscopes: stub out the protected WallpaperManager methods

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -77,6 +77,7 @@ import android.view.Display;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
+import com.android.internal.app.StorageScopesAppHooks;
 
 import libcore.io.IoUtils;
 
@@ -787,6 +788,10 @@ public class WallpaperManager {
      */
     @RequiresPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
     public Drawable getDrawable() {
+        if (StorageScopesAppHooks.shouldSpoofSelfPermissionCheck(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            return null;
+        }
+
         final ColorManagementProxy cmProxy = getColorManagementProxy();
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, true, FLAG_SYSTEM, cmProxy);
         if (bm != null) {
@@ -1044,6 +1049,10 @@ public class WallpaperManager {
      */
     @RequiresPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
     public Drawable getFastDrawable() {
+        if (StorageScopesAppHooks.shouldSpoofSelfPermissionCheck(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            return null;
+        }
+
         final ColorManagementProxy cmProxy = getColorManagementProxy();
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, true, FLAG_SYSTEM, cmProxy);
         if (bm != null) {
@@ -1061,6 +1070,10 @@ public class WallpaperManager {
      */
     @RequiresPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
     public Drawable peekFastDrawable() {
+        if (StorageScopesAppHooks.shouldSpoofSelfPermissionCheck(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            return null;
+        }
+
         final ColorManagementProxy cmProxy = getColorManagementProxy();
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, false, FLAG_SYSTEM, cmProxy);
         if (bm != null) {
@@ -1157,6 +1170,10 @@ public class WallpaperManager {
      */
     @RequiresPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
     public ParcelFileDescriptor getWallpaperFile(@SetWallpaperFlags int which) {
+        if (StorageScopesAppHooks.shouldSpoofSelfPermissionCheck(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            return null;
+        }
+
         return getWallpaperFile(which, mContext.getUserId());
     }
 
