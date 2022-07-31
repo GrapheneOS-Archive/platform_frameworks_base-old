0b02521a93508ac54196f4277700d3d9d7c148be
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 10:06:14 +0300

    Wi-Fi auto turn off
    Co-authored-by: Pratyush <codelab@pratyush.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12627,6 +12627,9 @@ public final class Settings {
         /** @hide */
         public static final String GNSS_PSDS_STANDARD = "psds_server"; // historical name
 
+        /** @hide */
+        public static final String WIFI_AUTO_OFF = "wifi_off_timeout";
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
