6e41040373a3246ce3830fed206cae2468f23242
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 18:00:35 +0300

    improve compatibility of INTERNET special runtime permission

--- core/java/android/content/pm/SrtPermissions.java
+++ core/java/android/content/pm/SrtPermissions.java
@@ -4,6 +4,8 @@ import android.Manifest;
 
 /** @hide */
 public class SrtPermissions { // "special runtime permissions"
+    public static final int FLAG_INTERNET_COMPAT_ENABLED = 1;
+
     private static int flags;
 
     public static int getFlags() {
@@ -12,10 +14,16 @@ public class SrtPermissions { // "special runtime permissions"
 
     public static void setFlags(int value) {
         flags = value;
+
+        if ((value & FLAG_INTERNET_COMPAT_ENABLED) != 0) {
+            SpecialRuntimePermAppUtils.enableInternetCompat();
+        }
     }
 
     public static boolean shouldSpoofSelfCheck(String permName) {
         switch (permName) {
+            case Manifest.permission.INTERNET:
+                return SpecialRuntimePermAppUtils.isInternetCompatEnabled();
             default:
                 return false;
         }
