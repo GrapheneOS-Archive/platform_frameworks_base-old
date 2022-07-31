503d8dc72be93829a276ff646d0ae9ac3fcebdf4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 18:00:35 +0300

    improve compatibility of INTERNET special runtime permission

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -34,6 +34,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 import android.database.DatabaseUtils;
+import android.database.MatrixCursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkPolicyManager;
 import android.net.Uri;
@@ -52,6 +53,8 @@ import android.text.TextUtils;
 import android.util.LongSparseArray;
 import android.util.Pair;
 import android.webkit.MimeTypeMap;
+
+import android.content.pm.SpecialRuntimePermAppUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -1115,6 +1118,11 @@ public class DownloadManager {
      * calls related to this download.
      */
     public long enqueue(Request request) {
+        if (SpecialRuntimePermAppUtils.isInternetCompatEnabled()) {
+            // invalid id (DownloadProvider uses SQLite and returns a row id)
+            return -1;
+        }
+
         ContentValues values = request.toContentValues(mPackageName);
         Uri downloadUri = mResolver.insert(Downloads.Impl.CONTENT_URI, values);
         long id = Long.parseLong(downloadUri.getLastPathSegment());
@@ -1147,6 +1155,11 @@ public class DownloadManager {
      * @return the number of downloads actually removed
      */
     public int remove(long... ids) {
+        if (SpecialRuntimePermAppUtils.isInternetCompatEnabled()) {
+            // underlying provider is protected by the INTERNET permission
+            return 0;
+        }
+
         return markRowDeleted(ids);
     }
 
@@ -1162,6 +1175,11 @@ public class DownloadManager {
 
     /** @hide */
     public Cursor query(Query query, String[] projection) {
+        if (SpecialRuntimePermAppUtils.isInternetCompatEnabled()) {
+            // underlying provider is protected by the INTERNET permission
+            return new MatrixCursor(projection);
+        }
+
         Cursor underlyingCursor = query.runQuery(mResolver, projection, mBaseUri);
         if (underlyingCursor == null) {
             return null;
@@ -1538,6 +1556,11 @@ public class DownloadManager {
         validateArgumentIsNonEmpty("mimeType", mimeType);
         if (length < 0) {
             throw new IllegalArgumentException(" invalid value for param: totalBytes");
+        }
+
+        if (SpecialRuntimePermAppUtils.isInternetCompatEnabled()) {
+            // underlying provider is protected by the INTERNET permission
+            return -1;
         }
 
         // if there is already an entry with the given path name in downloads.db, return its id
