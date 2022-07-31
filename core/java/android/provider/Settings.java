bf9a636d2e429dc8ab811d962915fdf53d8df77e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 11:19:33 +0300

    Bluetooth auto turn off
    Co-authored-by: Pratyush <codelab@pratyush.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17069,6 +17069,12 @@ public final class Settings {
         public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
 
         /**
+         * The amount of time in milliseconds before a disconnected Bluetooth adapter is turned off
+         * @hide
+         */
+        public static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+
+        /**
          * Settings migrated from Wear OS settings provider.
          * @hide
          */
