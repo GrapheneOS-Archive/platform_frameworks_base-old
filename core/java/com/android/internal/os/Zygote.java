6a2dd174ac8741eb971f11cc029a451bdc018e16
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-14 15:35:44 +0300

    add a wrapper for execveat(2)
    Needed for exec spawning, to pass custom flags to the kernel.

--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -1465,4 +1465,6 @@ public final class Zygote {
      * @hide
      */
     public static native void nativeHandleRuntimeFlags(int runtimeFlags);
+
+    public static native int execveatWrapper(int dirFd, String filename, String[] argv, int flags);
 }
