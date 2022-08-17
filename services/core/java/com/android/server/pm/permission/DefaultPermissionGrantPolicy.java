7fe05dfcbf90e1d6d30c9f761843039b327575e3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-17 11:49:44 +0300

    gmscompat: pre-grant POST_NOTIFICATIONS to GmsCompatApp

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -811,7 +811,8 @@ final class DefaultPermissionGrantPolicy {
                         ACTIVITY_RECOGNITION_PERMISSIONS);
             }
         }
-        grantPermissionsToSystemPackage(pm, GmsCompatApp.PKG_NAME, userId, ALWAYS_LOCATION_PERMISSIONS);
+        grantPermissionsToSystemPackage(pm, GmsCompatApp.PKG_NAME, userId,
+                ALWAYS_LOCATION_PERMISSIONS, NOTIFICATION_PERMISSIONS);
 
         // Music
         Intent musicIntent = new Intent(Intent.ACTION_VIEW)
