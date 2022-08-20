3f4a6517c35741b5b2adf5afdb2ba74598896a70
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-20 13:14:20 +0300

    gmscompat: Android 13 shims, part 3

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -31,6 +31,7 @@ import android.annotation.TestApi;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.compat.CompatChanges;
+import android.app.compat.gms.GmsCompat;
 import android.bluetooth.BluetoothCodecConfig;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothLeAudioCodecConfig;
@@ -8203,6 +8204,11 @@ public class AudioManager {
         }
         Objects.requireNonNull(device);
         Objects.requireNonNull(timeUnit);
+
+        if (GmsCompat.isEnabled()) {
+            return;
+        }
+
         try {
             getService().muteAwaitConnection(usagesToMute, device, timeUnit.toMillis(timeout));
         } catch (RemoteException e) {
@@ -8323,6 +8329,10 @@ public class AudioManager {
     public void registerMuteAwaitConnectionCallback(
             @NonNull @CallbackExecutor Executor executor,
             @NonNull MuteAwaitConnectionCallback callback) {
+        if (GmsCompat.isEnabled()) {
+            return;
+        }
+
         synchronized (mMuteAwaitConnectionListenerLock) {
             final Pair<ArrayList<ListenerInfo<MuteAwaitConnectionCallback>>,
                     MuteAwaitConnectionDispatcherStub> res =
