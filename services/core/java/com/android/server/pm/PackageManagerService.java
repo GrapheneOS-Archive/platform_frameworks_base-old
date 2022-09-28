21251cbf7576ee6149aed396eb6be425a9f400bd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-09-28 17:47:29 +0300

    gosps: return internal version in PackageManagerInternal

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -215,6 +215,7 @@ import com.android.server.pm.permission.LegacyPermissionManagerService;
 import com.android.server.pm.permission.PermissionManagerService;
 import com.android.server.pm.permission.PermissionManagerServiceInternal;
 import com.android.server.pm.permission.SpecialRuntimePermUtils;
+import com.android.server.pm.pkg.GosPackageStatePm;
 import com.android.server.pm.pkg.PackageStateInternal;
 import com.android.server.pm.pkg.PackageUserState;
 import com.android.server.pm.pkg.PackageUserStateInternal;
@@ -6529,8 +6530,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
 
         @Nullable
         @Override
-        public GosPackageState getGosPackageState(String packageName, int userId) {
-            return GosPackageStatePmHooks.get(PackageManagerService.this, packageName, userId);
+        public GosPackageStatePm getGosPackageState(String packageName, int userId) {
+            return GosPackageStatePm.get(PackageManagerService.this, packageName, userId);
         }
     }
 
