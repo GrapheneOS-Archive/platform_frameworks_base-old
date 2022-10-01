551c694c46b3a553620f43d0a168e2968457ca7f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-01 15:30:06 +0300

    gmscompat: improve infrastructure for PhenotypeFlag overrides
    Now that ProcessStablePhenotypeFlags are no longer supported, there's no need to directly intercept
    database queries.
    Reverting back to the interception at the higher (ContentProvider) level (the way it was done before
    GmsCompatConfig) allows to override flags that aren't present in phenotype.db.

--- core/java/android/database/sqlite/SQLiteDatabase.java
+++ core/java/android/database/sqlite/SQLiteDatabase.java
@@ -24,7 +24,6 @@ import android.annotation.StringDef;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.app.ActivityThread;
-import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -45,8 +44,6 @@ import android.util.EventLog;
 import android.util.Log;
 import android.util.Pair;
 import android.util.Printer;
-
-import com.android.internal.gmscompat.GmsHooks;
 import com.android.internal.util.Preconditions;
 import dalvik.system.CloseGuard;
 import java.io.File;
@@ -1555,24 +1552,15 @@ public final class SQLiteDatabase extends SQLiteClosable {
             String selection, String[] selectionArgs, String groupBy,
             String having, String orderBy, String limit, CancellationSignal cancellationSignal) {
         acquireReference();
-        Cursor res;
         try {
             String sql = SQLiteQueryBuilder.buildQueryString(
                     distinct, table, columns, selection, groupBy, having, orderBy, limit);
 
-            res = rawQueryWithFactory(cursorFactory, sql, selectionArgs,
+            return rawQueryWithFactory(cursorFactory, sql, selectionArgs,
                     findEditTable(table), cancellationSignal);
         } finally {
             releaseReference();
         }
-
-        if (GmsCompat.isGmsCore()) {
-            Cursor c = GmsHooks.maybeWrapSqlCursor(this, table, selection, selectionArgs, res);
-            if (c != null) {
-                return c;
-            }
-        }
-        return res;
     }
 
     /**
