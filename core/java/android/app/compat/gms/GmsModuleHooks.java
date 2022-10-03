ecae6a8a4c662d0b6ab4f5392e2ec856c0803dfd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-03 10:22:52 +0300

    gmscompat: support intercepting SynchronousResultReceiver exceptions
    SynchronousResultReceiver is used in many privileged Bluetooth-related methods.

--- core/java/android/app/compat/gms/GmsModuleHooks.java
+++ core/java/android/app/compat/gms/GmsModuleHooks.java
@@ -17,11 +17,16 @@
 package android.app.compat.gms;
 
 import android.Manifest;
+import android.annotation.NonNull;
 import android.annotation.SystemApi;
 import android.app.ActivityThread;
+import android.os.Build;
 import android.os.RemoteException;
+import android.util.Log;
 
 import com.android.internal.gmscompat.GmsCompatApp;
+import com.android.internal.gmscompat.GmsHooks;
+import com.android.internal.gmscompat.StubDef;
 
 /**
  * Hooks that are accessed from APEX modules.
@@ -30,6 +35,7 @@ import com.android.internal.gmscompat.GmsCompatApp;
  */
 @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
 public class GmsModuleHooks {
+    private static final String TAG = "GmsCompat/MHooks";
 
     // BluetoothAdapter#enable()
     // BluetoothAdapter#enableBLE()
@@ -48,6 +54,32 @@ public class GmsModuleHooks {
         } // else don't bother the user
 
         return false;
+    }
+
+    // com.android.modules.utils.SynchronousResultReceiver.Result#getValue()
+    public static boolean interceptSynchronousResultReceiverException(@NonNull RuntimeException origException) {
+        if (!(origException instanceof SecurityException)) {
+            return false;
+        }
+
+        // origException contains service-side stack trace, need to obtain an app-side one
+        var stackTrace = new Throwable();
+        StubDef stub = StubDef.find(stackTrace, GmsHooks.config());
+
+        if (stub == null) {
+            return false;
+        }
+
+        if (stub.type != StubDef.DEFAULT) {
+            Log.d(TAG, "interceptSynchronousResultReceiverException: unexpected stub type " + stub.type, stackTrace);
+            return false;
+        }
+
+        if (Build.isDebuggable()) {
+            Log.i(TAG, "intercepted " + origException, stackTrace);
+        }
+
+        return true;
     }
 
     private GmsModuleHooks() {}
