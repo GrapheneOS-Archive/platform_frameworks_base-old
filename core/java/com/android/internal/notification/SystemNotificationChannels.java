bf44feed7cb8a21e0d83288cff972871f31b1c27
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-04 18:02:35 +0300

    notify the user when sensors access is denied by OTHER_SENSORS perm
    Notification is not shown if OTHER_SENSORS was explicitly denied by the user.

--- core/java/com/android/internal/notification/SystemNotificationChannels.java
+++ core/java/com/android/internal/notification/SystemNotificationChannels.java
@@ -233,6 +233,8 @@ public class SystemNotificationChannels {
                 NotificationManager.IMPORTANCE_LOW);
         channelsList.add(abusiveBackgroundAppsChannel);
 
+        extraChannels(context, channelsList);
+
         nm.createNotificationChannels(channelsList);
     }
 
@@ -267,4 +269,22 @@ public class SystemNotificationChannels {
     }
 
     private SystemNotificationChannels() {}
+
+    public static final String MISSING_PERMISSION = "MISSING_PERMISSION";
+
+    private static void extraChannels(Context ctx, List<NotificationChannel> dest) {
+        channel(ctx, MISSING_PERMISSION,
+                    R.string.notification_channel_missing_permission,
+                    NotificationManager.IMPORTANCE_HIGH, true, dest);
+    }
+
+    private static NotificationChannel channel(Context ctx, String id, int nameRes, int importance, boolean silent, List<NotificationChannel> dest) {
+        var c = new NotificationChannel(id, ctx.getText(nameRes), importance);
+        if (silent) {
+            c.setSound(null, null);
+            c.enableVibration(false);
+        }
+        dest.add(c);
+        return c;
+    }
 }
