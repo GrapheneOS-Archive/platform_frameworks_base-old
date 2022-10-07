cd4e27fca834163fc14211bb94f3189958676830
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-07 20:46:16 +0300

    srt permissions: an ability to skip auto-grants for specific package

--- services/core/java/com/android/server/pm/permission/SpecialRuntimePermUtils.java
+++ services/core/java/com/android/server/pm/permission/SpecialRuntimePermUtils.java
@@ -19,11 +19,15 @@ package com.android.server.pm.permission;
 import android.Manifest;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageManagerInternal;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.LruCache;
 import android.util.Slog;
+import android.util.SparseArray;
 
+import com.android.server.LocalServices;
 import com.android.server.ext.SystemServerExt;
 import com.android.server.pm.parsing.pkg.AndroidPackage;
 import com.android.server.pm.pkg.component.ParsedUsesPermission;
@@ -76,7 +80,7 @@ public class SpecialRuntimePermUtils {
             }
         }
 
-        return true;
+        return !isAutoGrantSkipped(packageName, userId, perm);
     }
 
     public static int getFlags(AndroidPackage pkg) {
@@ -108,6 +112,41 @@ public class SpecialRuntimePermUtils {
         }
 
         return flags;
+    }
+
+    // Maps userIds to map of package names to permissions that should not be auto granted
+    private static SparseArray<LruCache<String, List<String>>> skipAutoGrantsMap = new SparseArray<>();
+
+    public static void skipAutoGrantsForPackage(String packageName, int userId, List<String> perms) {
+        var pkg = LocalServices.getService(PackageManagerInternal.class).getPackage(packageName);
+        if (pkg != null && pkg.isSystem()) {
+            return;
+        }
+
+        synchronized (skipAutoGrantsMap) {
+            LruCache<String, List<String>> userMap = skipAutoGrantsMap.get(userId);
+            if (userMap == null) {
+                // 50 entries should be enough, only 1 is needed in vast majority of cases
+                userMap = new LruCache<>(50);
+                skipAutoGrantsMap.put(userId, userMap);
+            }
+            userMap.put(packageName, perms);
+        }
+    }
+
+    private static boolean isAutoGrantSkipped(String packageName, int userId, String perm) {
+        List<String> permList;
+        synchronized (skipAutoGrantsMap) {
+            LruCache<String, List<String>> userMap = skipAutoGrantsMap.get(userId);
+            if (userMap == null) {
+                return false;
+            }
+            permList = userMap.get(packageName);
+        }
+        if (permList == null) {
+            return false;
+        }
+        return permList.contains(perm);
     }
 
     private SpecialRuntimePermUtils() {}
