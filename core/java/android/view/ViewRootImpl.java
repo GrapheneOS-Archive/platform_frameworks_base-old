dcac82150ca2883af476fda9ff90b4042f8d9615
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-11-14 11:57:00 +0200

    enable the post-upgrade "Optimizing apps" progress indication UI
    Adjustment to ViewRootImpl is needed because AudioService is not yet started when this UI is shown,
    which leads to a system_server crash.
    
    Caching the result of AudioManager.areNavigationRepeatSoundEffectsEnabled() is not necessary, this
    method is fast and its result is used only for sound effects when using keyboard for navigation.

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -405,8 +405,6 @@ public final class ViewRootImpl implements ViewParent,
     private boolean mUseBLASTAdapter;
     private boolean mForceDisableBLAST;
 
-    private boolean mFastScrollSoundEffectsEnabled;
-
     /**
      * Signals that compatibility booleans have been initialized according to
      * target SDK versions.
@@ -1000,8 +998,6 @@ public final class ViewRootImpl implements ViewParent,
 
         loadSystemProperties();
         mImeFocusController = new ImeFocusController(this);
-        AudioManager audioManager = mContext.getSystemService(AudioManager.class);
-        mFastScrollSoundEffectsEnabled = audioManager.areNavigationRepeatSoundEffectsEnabled();
 
         mScrollCaptureRequestTimeout = SCROLL_CAPTURE_REQUEST_TIMEOUT_MILLIS;
         mOnBackInvokedDispatcher = new WindowOnBackInvokedDispatcher(
@@ -8539,8 +8535,8 @@ public final class ViewRootImpl implements ViewParent,
         try {
             final AudioManager audioManager = getAudioManager();
 
-            if (mFastScrollSoundEffectsEnabled
-                    && SoundEffectConstants.isNavigationRepeat(effectId)) {
+            if (SoundEffectConstants.isNavigationRepeat(effectId)
+                    && audioManager.areNavigationRepeatSoundEffectsEnabled()) {
                 audioManager.playSoundEffect(
                         SoundEffectConstants.nextNavigationRepeatSoundEffectId());
                 return;
