924db848739df2adedf3991548800e93e713fea7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-12-06 09:36:34 +0200

    SystemUI: under-display fingerprint scanner support
    Based on classes from SystemUIGoogle, package com.google.android.systemui.fingerprint

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -34,6 +34,7 @@ import com.android.systemui.assist.AssistModule;
 import com.android.systemui.biometrics.AlternateUdfpsTouchProvider;
 import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider;
 import com.android.systemui.biometrics.UdfpsDisplayModeProvider;
+import com.android.systemui.biometrics.UdfpsTouchProvider;
 import com.android.systemui.biometrics.dagger.BiometricsModule;
 import com.android.systemui.biometrics.dagger.UdfpsModule;
 import com.android.systemui.classifier.FalsingModule;
@@ -260,6 +261,10 @@ public abstract class SystemUIModule {
 
     @BindsOptionalOf
     abstract SystemStatusAnimationScheduler optionalSystemStatusAnimationScheduler();
+
+    @SysUISingleton
+    @Binds
+    abstract AlternateUdfpsTouchProvider bindUdfpsTouchProvider(UdfpsTouchProvider provider);
 
     @SysUISingleton
     @Binds
