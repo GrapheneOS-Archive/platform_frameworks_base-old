20693561ab3c85f4d227ba11021e990ce952e411
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-01-20 11:42:11 +0200

    bugfix: isServiceTokenValidLocked() was called without holding the lock
    This method iterates through a list that is modified by multiple threads, which is not safe to do
    without locking.

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -10413,7 +10413,7 @@ public class NotificationManagerService extends SystemService {
      * given NAS is bound in.
      */
     private boolean isInteractionVisibleToListener(ManagedServiceInfo info, int userId) {
-        boolean isAssistantService = mAssistants.isServiceTokenValidLocked(info.service);
+        boolean isAssistantService = mAssistants.isServiceTokenValid(info.service);
         return !isAssistantService || info.isSameUser(userId);
     }
 
@@ -11688,7 +11688,7 @@ public class NotificationManagerService extends SystemService {
                 BackgroundThread.getHandler().post(() -> {
                     if (info.isSystem
                             || hasCompanionDevice(info)
-                            || mAssistants.isServiceTokenValidLocked(info.service)) {
+                            || mAssistants.isServiceTokenValid(info.service)) {
                         notifyNotificationChannelChanged(
                                 info, pkg, user, channel, modificationType);
                     }
