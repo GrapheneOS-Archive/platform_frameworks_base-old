ff6dbc07a4d1bf7d40e0991ba7cc4a2609bb55fa
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-01-20 10:29:58 +0200

    fully disallow installing instant apps

--- services/core/java/com/android/server/pm/PackageInstallerService.java
+++ services/core/java/com/android/server/pm/PackageInstallerService.java
@@ -729,12 +729,8 @@ public class PackageInstallerService extends IPackageInstaller.Stub implements
             }
         }
 
-        if ((params.installFlags & PackageManager.INSTALL_INSTANT_APP) != 0
-                && !isCalledBySystemOrShell(callingUid)
-                && (snapshot.getFlagsForUid(callingUid) & ApplicationInfo.FLAG_SYSTEM)
-                == 0) {
-            throw new SecurityException(
-                    "Only system apps could use the PackageManager.INSTALL_INSTANT_APP flag.");
+        if ((params.installFlags & PackageManager.INSTALL_INSTANT_APP) != 0) {
+            throw new SecurityException("Installing instant apps is forbidden");
         }
 
         if (params.isStaged && !isCalledBySystemOrShell(callingUid)) {
