c6adc76fd83cfb25e6d333b338d67e28ee81cd12
Daniel Micay <danielmicay@gmail.com>
2023-01-30 19:17:59 -0500

    Revert "Allow owner user to install apps from one secondary user to other."
    This reverts commit 0ddc8f7b0880d7064beef31fe7b3e950d2c7408c.
    
    This addition is problematic in a case where the secondary user which
    installed the app is not actually the same person as the owner of the
    device and has installed a malicious app.

--- packages/SettingsLib/src/com/android/settingslib/users/AppCopyHelper.java
+++ packages/SettingsLib/src/com/android/settingslib/users/AppCopyHelper.java
@@ -150,12 +150,10 @@ public class AppCopyHelper {
         addSystemApps(mVisibleApps, widgetIntent);
 
         // Add all downloaded apps of the current user
-        final List<ApplicationInfo> installedApps =
-                mPackageManager.getInstalledApplications(
-                    PackageManager.ApplicationInfoFlags.of(PackageManager.MATCH_ANY_USER)
-                );
+        final List<ApplicationInfo> installedApps = mPackageManager.getInstalledApplications(0);
         for (ApplicationInfo app : installedApps) {
             // If it's not installed, skip
+            if ((app.flags & ApplicationInfo.FLAG_INSTALLED) == 0) continue;
 
             if ((app.flags & ApplicationInfo.FLAG_SYSTEM) == 0
                     && (app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
