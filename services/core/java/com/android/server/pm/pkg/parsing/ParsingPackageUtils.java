2d1f401a2d8a26bfd61c852bd99eb662b0a6b173
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-09 19:02:24 +0200

    add special runtime permission for other sensors
    Ported from 12: 9d5a62ed573bc3c7be8b19445b372fed13533d0e

--- services/core/java/com/android/server/pm/pkg/parsing/ParsingPackageUtils.java
+++ services/core/java/com/android/server/pm/pkg/parsing/ParsingPackageUtils.java
@@ -2224,6 +2224,8 @@ public class ParsingPackageUtils {
 
         pkg.setHasDomainUrls(hasDomainURLs(pkg));
 
+        pkg.addUsesPermission(new ParsedUsesPermissionImpl(android.Manifest.permission.OTHER_SENSORS, 0));
+
         return input.success(pkg);
     }
 
