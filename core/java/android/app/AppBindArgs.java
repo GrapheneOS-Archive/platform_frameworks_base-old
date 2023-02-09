95852d4a575e5be890e918b466dc9fa200630d29
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-09 18:53:11 +0200

    infrastructure for special runtime permissions
    13: e6761e05a9ee

--- core/java/android/app/AppBindArgs.java
+++ core/java/android/app/AppBindArgs.java
@@ -4,5 +4,7 @@ package android.app;
 public interface AppBindArgs {
     String KEY_FLAGS_ARRAY = "flagsArr";
 
+    int FLAGS_IDX_SPECIAL_RUNTIME_PERMISSIONS = 0;
+
     int FLAGS_ARRAY_LEN = 10;
 }
