7ff74137e7a7d0bb56c837d4a7dce66243e2fffe
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-09 18:53:11 +0200

    infrastructure for special runtime permissions

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -215,6 +215,7 @@ import com.android.server.pm.permission.LegacyPermissionManagerInternal;
 import com.android.server.pm.permission.LegacyPermissionManagerService;
 import com.android.server.pm.permission.PermissionManagerService;
 import com.android.server.pm.permission.PermissionManagerServiceInternal;
+import com.android.server.pm.permission.SpecialRuntimePermUtils;
 import com.android.server.pm.pkg.PackageStateInternal;
 import com.android.server.pm.pkg.PackageUserState;
 import com.android.server.pm.pkg.PackageUserStateInternal;
@@ -6038,6 +6039,12 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         @Override
         public Bundle getExtraAppBindArgs(String packageName) {
             return PackageManagerHooks.getExtraAppBindArgs(PackageManagerService.this, packageName);
+        }
+
+        @Override
+        public void skipSpecialRuntimePermissionAutoGrantsForPackage(String packageName, int userId, List<String> permissions) {
+            mContext.enforceCallingPermission(Manifest.permission.INSTALL_PACKAGES, null);
+            SpecialRuntimePermUtils.skipAutoGrantsForPackage(packageName, userId, permissions);
         }
     }
 
