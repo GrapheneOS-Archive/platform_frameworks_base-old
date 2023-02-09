f6fa0c3a1b9ed2622731a7fe19f79b9fa44b8659
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-09 19:02:24 +0200

    add special runtime permission for other sensors
    Ported from 12: 9d5a62ed573bc3c7be8b19445b372fed13533d0e

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -3,5 +3,8 @@ package android.ext.settings;
 /** @hide */
 public class ExtSettings {
 
+    public static final BoolSetting AUTO_GRANT_OTHER_SENSORS_PERMISSION = new BoolSetting(
+            Setting.Scope.PER_USER, "auto_grant_OTHER_SENSORS_perm", true);
+
     private ExtSettings() {}
 }
