59a48b703023e39aee8eed7accbc6fb0073ed725
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-12 19:38:02 +0200

    a set of helper classes for defining system settings

---
+++ core/java/android/ext/settings/ExtSettings.java
@@ -0,0 +1,31 @@
+package android.ext.settings;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+/** @hide */
+public class ExtSettings {
+
+    private ExtSettings() {}
+
+    // used for making settings defined in this class unreadable by third-party apps
+    public static void getKeys(Setting.Scope scope, Set<String> dest) {
+        for (Field field : ExtSettings.class.getDeclaredFields()) {
+            if (!Setting.class.isAssignableFrom(field.getType())) {
+                continue;
+            }
+            Setting s;
+            try {
+                s = (Setting) field.get(null);
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException(e);
+            }
+
+            if (s.getScope() == scope) {
+                if (!dest.add(s.getKey())) {
+                    throw new IllegalStateException("duplicate definition of setting " + s.getKey());
+                }
+            }
+        }
+    }
+}
