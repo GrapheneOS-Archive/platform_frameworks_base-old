2256ca5fb40f4e1ef027af6de35926627e6f0884
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-12 19:38:02 +0200

    a set of helper classes for defining system settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -52,6 +52,8 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.ext.settings.ExtSettings;
+import android.ext.settings.Setting;
 import android.location.ILocationManager;
 import android.location.LocationManager;
 import android.media.AudioManager;
@@ -3500,6 +3502,17 @@ public final class Settings {
                         keysWithMaxTargetSdk.put(key, maxTargetSdk);
                     }
                 }
+            }
+
+            Setting.Scope extSettingsScope = null;
+            if (callerClass == Global.class) {
+                extSettingsScope = Setting.Scope.GLOBAL;
+            } else if (callerClass == Secure.class) {
+                extSettingsScope = Setting.Scope.PER_USER;
+            }
+
+            if (extSettingsScope != null) {
+                ExtSettings.getKeys(extSettingsScope, allKeys);
             }
         } catch (IllegalAccessException ignored) {
         }
