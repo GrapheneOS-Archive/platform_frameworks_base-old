b1d3b97543311f20d92bbc7038580bf91964f093
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-13 17:59:45 +0200

    always preserve package enabled setting after package update
    13: a2b998919c90

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -2292,16 +2292,6 @@ final class InstallPackageHelper {
                     if (DEBUG_INSTALL) {
                         Slog.d(TAG, "Implicitly enabling system package on upgrade: " + pkgName);
                     }
-                    // Enable system package for requested users
-                    if (installedForUsers != null
-                            && !installRequest.isApplicationEnabledSettingPersistent()) {
-                        for (int origUserId : installedForUsers) {
-                            if (userId == UserHandle.USER_ALL || userId == origUserId) {
-                                ps.setEnabled(COMPONENT_ENABLED_STATE_DEFAULT,
-                                        origUserId, installerPackageName);
-                            }
-                        }
-                    }
                     // Also convey the prior install/uninstall state
                     if (allUsers != null && installedForUsers != null) {
                         for (int currentUserId : allUsers) {
@@ -2347,10 +2337,6 @@ final class InstallPackageHelper {
                     // be installed and enabled. The caller, however, can explicitly specify to
                     // keep the existing enabled state.
                     ps.setInstalled(true, userId);
-                    if (!installRequest.isApplicationEnabledSettingPersistent()) {
-                        ps.setEnabled(COMPONENT_ENABLED_STATE_DEFAULT, userId,
-                                installerPackageName);
-                    }
                     // Clear any existing archive state.
                     mPm.mInstallerService.mPackageArchiver.clearArchiveState(ps, userId);
                 } else if (allUsers != null) {
@@ -2371,10 +2357,6 @@ final class InstallPackageHelper {
                                         UserManager.DISALLOW_DEBUGGING_FEATURES);
                         if (installedForCurrentUser || !restrictedByPolicy) {
                             ps.setInstalled(true, currentUserId);
-                            if (!installRequest.isApplicationEnabledSettingPersistent()) {
-                                ps.setEnabled(COMPONENT_ENABLED_STATE_DEFAULT, currentUserId,
-                                        installerPackageName);
-                            }
                             // Clear any existing archive state.
                             mPm.mInstallerService.mPackageArchiver.clearArchiveState(ps,
                                     currentUserId);
