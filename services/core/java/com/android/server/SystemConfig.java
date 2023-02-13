666287d6c9126c92965e8b190b12343365f74568
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-13 17:59:45 +0200

    always preserve package enabled setting after package update
    13: a2b998919c90

--- services/core/java/com/android/server/SystemConfig.java
+++ services/core/java/com/android/server/SystemConfig.java
@@ -1543,6 +1543,8 @@ public class SystemConfig {
             }
         }
 
+        addFeature("grapheneos.package_update_preserves_package_enabled_setting", 0);
+
         for (String featureName : mUnavailableFeatures) {
             removeFeature(featureName);
         }
