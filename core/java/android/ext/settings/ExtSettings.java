3ad6fdf60ead7ead16ff07f902fedfa15bb2edc4
r3g_5z <june@girlboss.ceo>
2023-02-18 13:12:40 -0500

    add toggle for screenshot timestamp EXIF metadata
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -39,6 +39,9 @@ public class ExtSettings {
             // default value: 18 hours
             (int) TimeUnit.HOURS.toMillis(18));
 
+    public static final BoolSetting SCREENSHOT_TIMESTAMP_EXIF = new BoolSetting(
+            Setting.Scope.PER_USER, Settings.Secure.SCREENSHOT_TIMESTAMP_EXIF, false);
+
     // AppCompatConfig specifies which hardening features are compatible/incompatible with a
     // specific app.
     // This setting controls whether incompatible hardening features would be disabled by default
