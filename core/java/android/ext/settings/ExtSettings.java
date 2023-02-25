fba35bb31ab4dc8bda05e65b5f9da554dcc94f70
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-25 12:05:48 +0200

    make settings defined in ExtSettings unreadable by third-party apps
    Applies to Settings.{Global,Secure} settings. System properties don't have this kind of access checks.

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -1,5 +1,8 @@
 package android.ext.settings;
 
+import java.lang.reflect.Field;
+import java.util.Set;
+
 /** @hide */
 public class ExtSettings {
 
@@ -16,4 +19,25 @@ public class ExtSettings {
             Setting.Scope.PER_USER, "screenshot_timestamp_exif", false);
 
     private ExtSettings() {}
+
+    // used for making settings defined in this class unreadable by third-party apps
+    public static void getKeys(Setting.Scope scope, Set<String> dest) {
+        for (Field field : ExtSettings.class.getDeclaredFields()) {
+            if (!Setting.class.isAssignableFrom(field.getType())) {
+                continue;
+            }
+            Setting s;
+            try {
+                s = (Setting) field.get(null);
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException(e);
+            }
+
+            if (s.getScope() == scope) {
+                if (!dest.add(s.getKey())) {
+                    throw new IllegalStateException("duplicate definition of setting " + s.getKey());
+                }
+            }
+        }
+    }
 }
