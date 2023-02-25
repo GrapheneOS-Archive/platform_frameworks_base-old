1937fd13c2b9d936ef6276a6e2ddaaf1ec009921
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-25 18:41:45 +0200

    copy isSystem() property into result of PackageManager.findPackage()
    Needed for handling initial install of a system package in Apps app.
    
    Squash into "allow privileged installer to search for packages across all users"

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6088,6 +6088,9 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             pi.applicationInfo.setBaseCodePath(pkg.getBaseApkPath());
             pi.applicationInfo.setSplitCodePaths(pkg.getSplitCodePaths());
 
+            if (pkg.isSystem()) {
+                pi.applicationInfo.flags |= ApplicationInfo.FLAG_SYSTEM;
+            }
             return pi;
         }
     }
