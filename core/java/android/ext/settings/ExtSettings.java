31f2b32eb22b337ee5199b4e264933e7f6c04a66
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-03 08:57:45 +0200

    add GNSS SUPL setting

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -4,6 +4,10 @@ import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import static android.ext.settings.GnssConstants.SUPL_DISABLED;
+import static android.ext.settings.GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY;
+import static android.ext.settings.GnssConstants.SUPL_SERVER_STANDARD;
+
 /** @hide */
 public class ExtSettings {
 
@@ -23,6 +27,12 @@ public class ExtSettings {
 
     public static final BoolSetting SCREENSHOT_TIMESTAMP_EXIF = new BoolSetting(
             Setting.Scope.PER_USER, "screenshot_timestamp_exif", false);
+
+    public static final IntSetting GNSS_SUPL = new IntSetting(
+            Setting.Scope.GLOBAL, "force_disable_supl", // historical name
+            SUPL_SERVER_GRAPHENEOS_PROXY, // default
+            SUPL_SERVER_STANDARD, SUPL_DISABLED, SUPL_SERVER_GRAPHENEOS_PROXY // valid values
+    );
 
     private ExtSettings() {}
 
