a822ca160ab72c876e8452cd54905edf96854d7e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-03 08:57:45 +0200

    add GNSS SUPL setting

---
+++ core/java/android/ext/settings/GnssConstants.java
@@ -0,0 +1,8 @@
+package android.ext.settings;
+
+/** @hide */
+public interface GnssConstants {
+    int SUPL_SERVER_STANDARD = 0;
+    int SUPL_DISABLED = 1;
+    int SUPL_SERVER_GRAPHENEOS_PROXY = 2;
+}
