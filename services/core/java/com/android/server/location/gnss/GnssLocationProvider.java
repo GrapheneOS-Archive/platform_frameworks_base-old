9d15f3ac345fb68306d0c132c5c1d04a9d1e13a3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-03 08:57:45 +0200

    add GNSS SUPL setting
    13: 47eb7cbe1686

--- services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
+++ services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
@@ -60,6 +60,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.ContentObserver;
+import android.ext.settings.ExtSettings;
+import android.ext.settings.IntSetting;
 import android.location.GnssCapabilities;
 import android.location.GnssStatus;
 import android.location.INetInitiatedListener;
@@ -104,6 +106,7 @@ import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.util.Pair;
+import android.util.Slog;
 import android.util.TimeUtils;
 
 import com.android.internal.annotations.GuardedBy;
@@ -131,6 +134,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * A GNSS implementation of LocationProvider used by LocationManager.
@@ -489,6 +493,13 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         mGnssNative.setNotificationCallbacks(this);
         mGnssNative.setLocationRequestCallbacks(this);
         mGnssNative.setTimeCallbacks(this);
+
+        Consumer<IntSetting> suplSettingObserver = setting -> {
+            Slog.d(TAG, "SUPL setting changed, value: " + setting.get(mContext));
+            reloadGpsProperties();
+        };
+
+        ExtSettings.GNSS_SUPL.registerObserver(mContext, suplSettingObserver, mHandler);
     }
 
     /** Called when system is ready. */
