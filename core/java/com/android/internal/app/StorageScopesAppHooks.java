c0a645a8e7bf0fadeb061e272cf3c125d672263f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-08 18:54:57 +0200

    refactor self permission check spoofing in Storage Scopes
    This will simplify extending the list of conditions for spoofing self permission checks.
    
    Squash into "infrastructure for the Storage Scopes feature"

--- core/java/com/android/internal/app/StorageScopesAppHooks.java
+++ core/java/com/android/internal/app/StorageScopesAppHooks.java
@@ -16,17 +16,14 @@
 
 package com.android.internal.app;
 
-import android.annotation.NonNull;
-import android.annotation.Nullable;
+import android.Manifest;
 import android.app.AppGlobals;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.AppPermissionUtils;
 import android.content.pm.GosPackageState;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.Process;
 import android.provider.MediaStore;
 import android.provider.Settings;
 
@@ -35,61 +32,20 @@ import static android.content.pm.GosPackageState.*;
 public class StorageScopesAppHooks {
     private static final String TAG = "StorageScopesAppHooks";
 
-    private static boolean shouldSpoofPermissionCheck(@Nullable GosPackageState ps, int permDerivedFlag) {
-        if (ps == null) {
+    private static boolean shouldSpoofSelfPermissionCheck(int permDerivedFlag) {
+        if (permDerivedFlag == 0) {
             return false;
         }
-        return ps.hasFlag(FLAG_STORAGE_SCOPES_ENABLED) && ps.hasDerivedFlag(permDerivedFlag);
+        GosPackageState ps = GosPackageState.getForSelf();
+        return ps != null && ps.hasFlag(FLAG_STORAGE_SCOPES_ENABLED) && ps.hasDerivedFlag(permDerivedFlag);
     }
 
-    public static boolean shouldSpoofSelfPermissionCheck(@NonNull String permName) {
-        int permDflag = AppPermissionUtils.getSpoofableStorageRuntimePermissionDflag(permName);
-        if (permDflag != 0) {
-            return shouldSpoofPermissionCheck(GosPackageState.getForSelf(), permDflag);
-        }
-        return false;
-    }
-
-    private static int isSpoofableAppOp(int op) {
-        switch (op) {
-            case AppOpsManager.OP_READ_EXTERNAL_STORAGE:
-                return DFLAG_HAS_READ_EXTERNAL_STORAGE_DECLARATION;
-
-            case AppOpsManager.OP_WRITE_EXTERNAL_STORAGE:
-                return DFLAG_HAS_WRITE_EXTERNAL_STORAGE_DECLARATION;
-
-            case AppOpsManager.OP_READ_MEDIA_AUDIO:
-                return DFLAG_HAS_READ_MEDIA_AUDIO_DECLARATION;
-
-            case AppOpsManager.OP_READ_MEDIA_IMAGES:
-                return DFLAG_HAS_READ_MEDIA_IMAGES_DECLARATION;
-
-            case AppOpsManager.OP_READ_MEDIA_VIDEO:
-                return DFLAG_HAS_READ_MEDIA_VIDEO_DECLARATION;
-
-            case AppOpsManager.OP_MANAGE_EXTERNAL_STORAGE:
-                return DFLAG_HAS_MANAGE_EXTERNAL_STORAGE_DECLARATION;
-
-            case AppOpsManager.OP_MANAGE_MEDIA:
-                return DFLAG_HAS_MANAGE_MEDIA_DECLARATION;
-
-            case AppOpsManager.OP_ACCESS_MEDIA_LOCATION:
-                return DFLAG_HAS_ACCESS_MEDIA_LOCATION_DECLARATION;
-
-            default:
-                return 0;
-        }
+    public static boolean shouldSpoofSelfPermissionCheck(String permName) {
+        return shouldSpoofSelfPermissionCheck(getSpoofablePermissionDflag(permName));
     }
 
     public static boolean shouldSpoofSelfAppOpCheck(int op) {
-        int opDerivedFlag = isSpoofableAppOp(op);
-
-        if (opDerivedFlag == 0) {
-            return false;
-        }
-
-        GosPackageState ps = GosPackageState.getForSelf();
-        return ps != null && ps.hasFlag(FLAG_STORAGE_SCOPES_ENABLED) && ps.hasDerivedFlag(opDerivedFlag);
+        return shouldSpoofSelfPermissionCheck(getSpoofableAppOpPermissionDflag(op));
     }
 
     // Instrumentation#execStartActivity(Context, IBinder, IBinder, Activity, Intent, int, Bundle)
@@ -141,6 +97,62 @@ public class StorageScopesAppHooks {
 
         if (shouldModify) {
             i.setAction(action + "_PROMPT");
+        }
+    }
+
+    public static int getSpoofablePermissionDflag(String permName) {
+        switch (permName) {
+            case Manifest.permission.READ_EXTERNAL_STORAGE:
+                return DFLAG_HAS_READ_EXTERNAL_STORAGE_DECLARATION;
+
+            case Manifest.permission.WRITE_EXTERNAL_STORAGE:
+                return DFLAG_HAS_WRITE_EXTERNAL_STORAGE_DECLARATION;
+
+            case Manifest.permission.ACCESS_MEDIA_LOCATION:
+                return DFLAG_HAS_ACCESS_MEDIA_LOCATION_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_AUDIO:
+                return DFLAG_HAS_READ_MEDIA_AUDIO_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_IMAGES:
+                return DFLAG_HAS_READ_MEDIA_IMAGES_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_VIDEO:
+                return DFLAG_HAS_READ_MEDIA_VIDEO_DECLARATION;
+
+            default:
+                return 0;
+        }
+    }
+
+    private static int getSpoofableAppOpPermissionDflag(int op) {
+        switch (op) {
+            case AppOpsManager.OP_READ_EXTERNAL_STORAGE:
+                return DFLAG_HAS_READ_EXTERNAL_STORAGE_DECLARATION;
+
+            case AppOpsManager.OP_WRITE_EXTERNAL_STORAGE:
+                return DFLAG_HAS_WRITE_EXTERNAL_STORAGE_DECLARATION;
+
+            case AppOpsManager.OP_READ_MEDIA_AUDIO:
+                return DFLAG_HAS_READ_MEDIA_AUDIO_DECLARATION;
+
+            case AppOpsManager.OP_READ_MEDIA_IMAGES:
+                return DFLAG_HAS_READ_MEDIA_IMAGES_DECLARATION;
+
+            case AppOpsManager.OP_READ_MEDIA_VIDEO:
+                return DFLAG_HAS_READ_MEDIA_VIDEO_DECLARATION;
+
+            case AppOpsManager.OP_MANAGE_EXTERNAL_STORAGE:
+                return DFLAG_HAS_MANAGE_EXTERNAL_STORAGE_DECLARATION;
+
+            case AppOpsManager.OP_MANAGE_MEDIA:
+                return DFLAG_HAS_MANAGE_MEDIA_DECLARATION;
+
+            case AppOpsManager.OP_ACCESS_MEDIA_LOCATION:
+                return DFLAG_HAS_ACCESS_MEDIA_LOCATION_DECLARATION;
+
+            default:
+                return 0;
         }
     }
 
