f0389151f56e22c5bc1f91fd4443c15f8dea9c8e
r3g_5z <june@girlboss.ceo>
2023-03-19 10:30:29 -0400

    SystemUI: make screenshot sound use touch sounds setting
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -1049,6 +1049,10 @@ public class ScreenshotController {
     }
 
     private void playCameraSound() {
+        if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 0) != 1) {
+            return;
+        }
+
         mCameraSound.addListener(() -> {
             try {
                 MediaPlayer player = mCameraSound.get();
