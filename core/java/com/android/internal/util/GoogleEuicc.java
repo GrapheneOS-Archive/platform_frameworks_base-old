c2655bcb55349c8d396555e0f024bd903753a7d0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-27 16:29:13 +0300

    integrate Google's EuiccSupportPixel package
    Depends on commit: "don't crash apps that depend on missing Gservices provider"

---
+++ core/java/com/android/internal/util/GoogleEuicc.java
@@ -0,0 +1,6 @@
+package com.android.internal.util;
+
+public class GoogleEuicc {
+    // handles firmware updates of embedded secure element that is used for eSIM, NFC, Felica etc
+    public static final String EUICC_SUPPORT_PIXEL_PKG_NAME = "com.google.euiccpixel";
+}
