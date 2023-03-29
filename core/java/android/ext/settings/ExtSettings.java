fe39b6b5a9fb6355f65b8a67854ed3bc0afb46d6
flawedworld <flawedworld@flawed.world>
2023-03-29 18:23:41 +0000

    Default to automatically rebooting device after 72 hours

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -29,7 +29,9 @@ public class ExtSettings {
             Setting.Scope.PER_USER, "auto_grant_OTHER_SENSORS_perm", true);
 
     public static final IntSetting AUTO_REBOOT_TIMEOUT = new IntSetting(
-            Setting.Scope.GLOBAL, "settings_reboot_after_timeout", 0);
+            Setting.Scope.GLOBAL, "settings_reboot_after_timeout",
+            // default value: 3 days
+            (int) TimeUnit.DAYS.toMillis(3));
 
     private ExtSettings() {}
 
