685af009707de068c8f318ddee9dec96d0f15ded
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 17:42:56 +0300

    exec spawning: support runtime resource overlays

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18644,4 +18644,10 @@ public class ActivityManagerService extends IActivityManager.Stub
             Trace.traceBegin(traceTag, methodName + subInfo);
         }
     }
+
+    @Override
+    public String[] getSystemIdmapPaths() {
+        // see comment in AssetManager#createSystemAssetsInZygoteLocked()
+        return android.content.res.AssetManager.systemIdmapPaths_;
+    }
 }
