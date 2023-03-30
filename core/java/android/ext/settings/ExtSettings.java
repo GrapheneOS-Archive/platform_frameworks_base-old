c71fc5ee1a5d6ac27708de70370a0f8531daf413
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 17:28:38 +0300

    make default value of auto-reboot setting more readable

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -2,6 +2,7 @@ package android.ext.settings;
 
 import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static android.ext.settings.GnssConstants.SUPL_DISABLED;
 import static android.ext.settings.GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY;
@@ -19,7 +20,9 @@ public class ExtSettings {
             Setting.Scope.PER_USER, "auto_grant_OTHER_SENSORS_perm", true);
 
     public static final IntSetting AUTO_REBOOT_TIMEOUT = new IntSetting(
-            Setting.Scope.GLOBAL, "settings_reboot_after_timeout", 259200000);
+            Setting.Scope.GLOBAL, "settings_reboot_after_timeout",
+            // default value: 3 days
+            (int) TimeUnit.DAYS.toMillis(3));
 
     public static final BoolSetting SCREENSHOT_TIMESTAMP_EXIF = new BoolSetting(
             Setting.Scope.PER_USER, "screenshot_timestamp_exif", false);
