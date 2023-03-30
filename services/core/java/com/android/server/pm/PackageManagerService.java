d67338d3107bf2f87fc99ff2f9991ebd401ae86f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 10:35:08 +0300

    infrastructure for creating per-package seinfo overrides
    13: 9fdbd47fd5bf

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -215,6 +215,7 @@ import com.android.server.art.model.DeleteResult;
 import com.android.server.compat.CompatChange;
 import com.android.server.compat.PlatformCompat;
 import com.android.server.ext.PackageManagerHooks;
+import com.android.server.ext.SeInfoOverride;
 import com.android.server.pm.Installer.InstallerException;
 import com.android.server.pm.Installer.LegacyDexoptDisabledException;
 import com.android.server.pm.Settings.VersionInfo;
@@ -1789,6 +1790,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 selinuxChangeListener);
         injector.getCompatibility().registerListener(SELinuxMMAC.SELINUX_R_CHANGES,
                 selinuxChangeListener);
+
+        m.selinuxChangeListener = selinuxChangeListener;
 
         m.installAllowlistedSystemPackages();
         IPackageManagerImpl iPackageManager = m.new IPackageManagerImpl();
@@ -6584,6 +6587,11 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         public boolean updateListOfBusyPackages(boolean add, List<String> packageNames, IBinder callerBinder) {
             return privInstallerHelper.updateListOfBusyPackages(add, packageNames, callerBinder);
         }
+
+        @Override
+        public void updateSeInfo(String packageName) {
+            SeInfoOverride.updateSeInfo(PackageManagerService.this, packageName);
+        }
     }
 
     private class PackageManagerInternalImpl extends PackageManagerInternalBase {
@@ -8159,5 +8167,12 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         return mDeletePackageHelper.deletePackageX(packageName,
                 PackageManager.VERSION_CODE_HIGHEST, UserHandle.USER_SYSTEM,
                 PackageManager.DELETE_ALL_USERS, true /*removedBySystem*/);
+    }
+
+    private CompatChange.ChangeListener selinuxChangeListener;
+
+    public void updateSeInfo(String packageName) {
+        // use the same procedure that is used for SELinux compat changes
+        selinuxChangeListener.onCompatChange(packageName);
     }
 }
