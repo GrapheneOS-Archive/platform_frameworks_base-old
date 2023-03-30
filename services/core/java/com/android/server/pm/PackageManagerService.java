368a2270e33e3bee621afe680a815aa31a544e03
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 10:35:08 +0300

    infrastructure for creating per-package seinfo overrides

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -200,6 +200,7 @@ import com.android.server.apphibernation.AppHibernationManagerInternal;
 import com.android.server.compat.CompatChange;
 import com.android.server.compat.PlatformCompat;
 import com.android.server.ext.PackageManagerHooks;
+import com.android.server.ext.SeInfoOverride;
 import com.android.server.pm.Installer.InstallerException;
 import com.android.server.pm.Settings.VersionInfo;
 import com.android.server.pm.dex.ArtManagerService;
@@ -1535,6 +1536,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 selinuxChangeListener);
         injector.getCompatibility().registerListener(SELinuxMMAC.SELINUX_R_CHANGES,
                 selinuxChangeListener);
+
+        m.selinuxChangeListener = selinuxChangeListener;
 
         m.installAllowlistedSystemPackages();
         IPackageManagerImpl iPackageManager = m.new IPackageManagerImpl();
@@ -6127,6 +6130,11 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         public boolean updateListOfBusyPackages(boolean add, List<String> packageNames, IBinder callerBinder) {
             return privInstallerHelper.updateListOfBusyPackages(add, packageNames, callerBinder);
         }
+
+        @Override
+        public void updateSeInfo(String packageName) {
+            SeInfoOverride.updateSeInfo(PackageManagerService.this, packageName);
+        }
     }
 
     private class PackageManagerLocalImpl implements PackageManagerLocal {
@@ -7406,5 +7414,12 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         synchronized (mLock) {
             mSettings.addInstallerPackageNames(installSource);
         }
+    }
+
+    private CompatChange.ChangeListener selinuxChangeListener;
+
+    public void updateSeInfo(String packageName) {
+        // use the same procedure that is used for SELinux compat changes
+        selinuxChangeListener.onCompatChange(packageName);
     }
 }
