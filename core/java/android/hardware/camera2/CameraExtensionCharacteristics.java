1cc4ab71e2615321a02206246832443e0b6f8489
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 13:31:44 +0300

    require vendor camera extensions package to be a system app by default
    This is a precaution in case ro.vendor.camera.extensions.package is set to an unexpected value.

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -22,6 +22,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.ApplicationInfoFlags;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.extension.IAdvancedExtenderImpl;
 import android.hardware.camera2.extension.ICameraExtensionsProxyService;
@@ -249,6 +252,16 @@ public final class CameraExtensionCharacteristics {
             return GLOBAL_CAMERA_MANAGER;
         }
 
+        private static boolean validateVendorCameraExtensionsPackage(Context ctx, String pkgName) {
+            try {
+                ApplicationInfo ai = ctx.getPackageManager().getApplicationInfo(pkgName, 0);
+                return ai.isSystemApp();
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
         private void connectToProxyLocked(Context ctx) {
             if (mConnection == null) {
                 Intent intent = new Intent();
@@ -257,7 +270,8 @@ public final class CameraExtensionCharacteristics {
                     "ro.vendor.camera.extensions.package");
                 String vendorProxyService = SystemProperties.get(
                     "ro.vendor.camera.extensions.service");
-                if (!vendorProxyPackage.isEmpty() && !vendorProxyService.isEmpty()) {
+                if (!vendorProxyPackage.isEmpty() && !vendorProxyService.isEmpty()
+                        && validateVendorCameraExtensionsPackage(ctx, vendorProxyPackage)) {
                   Log.v(TAG,
                       "Choosing the vendor camera extensions proxy package: "
                       + vendorProxyPackage);
