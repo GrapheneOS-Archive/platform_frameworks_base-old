c3bef2deb7ce3eadf7625c4952b3d84e44d71c58
r3g_5z <june@girlboss.ceo>
2023-04-02 22:28:14 -0400

    attestation key provisioning server setting
    Co-authored-by: flawedworld <flawedworld@flawed.world>
    Signed-off-by: r3g_5z <june@girlboss.ceo>

---
+++ core/java/android/ext/settings/RemoteProvisioningConstants.java
@@ -0,0 +1,10 @@
+package android.ext.settings;
+
+/** @hide */
+public interface RemoteProvisioningConstants {
+    int GRAPHENEOS_PROXY = 0;
+    int STANDARD_SERVER = 1;
+    int DISABLED = 2; // unused for now
+
+    String GRAPHENEOS_PROXY_URL = "https://remoteprovisioning.grapheneos.org/v1";
+}
