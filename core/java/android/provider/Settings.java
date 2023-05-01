7dea7f308da33b53404979d175f9e55bf2dbfa5d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-01 11:17:29 +0300

    Revert "Add infrastructure for choosing PSDS server"
    This reverts commit 1324711a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17308,12 +17308,6 @@ public final class Settings {
 
 
         /**
-         * Which server to use for Broadcom GNSS almanacs
-         * @hide
-         */
-        public static final String PSDS_SERVER = "psds_server";
-
-        /**
          * The amount of time in milliseconds before a disconnected Wi-Fi adapter is turned off
          * @hide
          */
