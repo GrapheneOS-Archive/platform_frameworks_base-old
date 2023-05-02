18330644e334f30b2c2e4c95eba5cb33046a22ed
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-02 18:39:28 +0300

    add GNSS PSDS setting

--- core/java/android/ext/settings/GnssConstants.java
+++ core/java/android/ext/settings/GnssConstants.java
@@ -5,4 +5,14 @@ public interface GnssConstants {
     int SUPL_SERVER_STANDARD = 0;
     int SUPL_DISABLED = 1;
     int SUPL_SERVER_GRAPHENEOS_PROXY = 2;
+
+    String PSDS_TYPE_QUALCOMM_XTRA = "qualcomm_xtra";
+    String PSDS_TYPE_BROADCOM = "broadcom";
+
+    String PSDS_SERVER_GRAPHENEOS_BROADCOM = "https://broadcom.psds.grapheneos.org";
+
+    // keep in sync with bionic/libc/bionic/gnss_psds_setting.c
+    int PSDS_SERVER_GRAPHENEOS = 0;
+    int PSDS_SERVER_STANDARD = 1;
+    int PSDS_DISABLED = 2;
 }
