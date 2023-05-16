c74afba16f69adb683c511f16f72cd6982d0edcd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 14:27:15 +0300

    AppBindArgs: infrastructure for passing extra args to app process init

--- services/core/java/com/android/server/ext/PackageManagerHooks.java
+++ services/core/java/com/android/server/ext/PackageManagerHooks.java
@@ -3,9 +3,18 @@ package com.android.server.ext;
 import android.Manifest;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
+import android.app.AppBindArgs;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.UserHandle;
 import android.util.ArraySet;
 
+import com.android.server.pm.PackageManagerService;
+import com.android.server.pm.permission.Permission;
+import com.android.server.pm.pkg.AndroidPackage;
 import com.android.server.pm.pkg.PackageStateInternal;
 import com.android.server.pm.pkg.parsing.ParsingPackage;
 
@@ -24,6 +33,38 @@ public class PackageManagerHooks {
             PackageManagerInternal pm, String permName, String packageName, int userId)
     {
         return false;
+    }
+
+    @Nullable
+    public static Bundle getExtraAppBindArgs(PackageManagerService pm, String packageName) {
+        final int callingUid = Binder.getCallingUid();
+        final int appId = UserHandle.getAppId(callingUid);
+        final int userId = UserHandle.getUserId(callingUid);
+
+        PackageStateInternal pkgState = pm.snapshotComputer().getPackageStateInternal(packageName);
+        if (pkgState == null) {
+            return null;
+        }
+
+        if (pkgState.getAppId() != appId) {
+            return null;
+        }
+
+        AndroidPackage pkg = pkgState.getPkg();
+
+        if (pkg == null) {
+            return null;
+        }
+
+        // isSystem() remains true even if isUpdatedSystemApp() is true
+        final boolean isUserApp = !pkgState.isSystem();
+
+        int[] flagsArr = new int[AppBindArgs.FLAGS_ARRAY_LEN];
+
+        var b = new Bundle();
+        b.putIntArray(AppBindArgs.KEY_FLAGS_ARRAY, flagsArr);
+
+        return b;
     }
 
     // Called when AppsFilter decides whether to restrict package visibility
