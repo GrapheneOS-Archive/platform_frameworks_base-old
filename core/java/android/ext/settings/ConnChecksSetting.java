aaa5e5607d43a09bb7dfbd1ce7c0f225361b7db7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-25 10:44:08 +0300

    add connectivity checks setting and its migration from Settings.Global

---
+++ core/java/android/ext/settings/ConnChecksSetting.java
@@ -0,0 +1,32 @@
+package android.ext.settings;
+
+import android.annotation.SystemApi;
+import android.os.SystemProperties;
+
+/** @hide */
+@SystemApi
+public class ConnChecksSetting {
+
+    public static final int VAL_GRAPHENEOS = 0;
+    public static final int VAL_STANDARD = 1;
+    public static final int VAL_DISABLED = 2;
+    public static final int VAL_DEFAULT = VAL_GRAPHENEOS;
+
+    public static int get() {
+        return ExtSettings.CONNECTIVITY_CHECKS.get();
+    }
+
+    public static boolean put(int val) {
+        return ExtSettings.CONNECTIVITY_CHECKS.put(val);
+    }
+
+    /**
+     * Use only during migration, to decide whether to perform it.
+     * @hide
+     */
+    public static boolean isSet() {
+        return !SystemProperties.get(ExtSettings.CONNECTIVITY_CHECKS.getKey()).isEmpty();
+    }
+
+    private ConnChecksSetting() {}
+}
