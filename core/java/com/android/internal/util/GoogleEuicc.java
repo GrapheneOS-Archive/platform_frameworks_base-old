20f544fd7c653aa69a65669abe94a40f47a8ccae
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-30 17:00:09 +0300

    integrate Google's eUICC LPA app
    Depends on "integrate Google's EuiccSupportPixel package" commit and on GmsCompat.
    
    Changes to data/etc/preinstalled-packages-platform.xml authored by flawedworld.
    
    Co-authored-by: flawedworld <flawedworld@flawed.world>

--- core/java/com/android/internal/util/GoogleEuicc.java
+++ core/java/com/android/internal/util/GoogleEuicc.java
@@ -1,6 +1,32 @@
 package com.android.internal.util;
 
+import android.app.compat.gms.GmsCompat;
+import android.os.UserHandle;
+
+import com.android.internal.gmscompat.GmsInfo;
+
 public class GoogleEuicc {
     // handles firmware updates of embedded secure element that is used for eSIM, NFC, Felica etc
     public static final String EUICC_SUPPORT_PIXEL_PKG_NAME = "com.google.euiccpixel";
+    public static final String LPA_PKG_NAME = "com.google.android.euicc";
+
+    public static String[] getLpaDependencies() {
+        return new String[] {
+            GmsInfo.PACKAGE_GSF, GmsInfo.PACKAGE_GMS_CORE, GmsInfo.PACKAGE_PLAY_STORE,
+        };
+    }
+
+    public static boolean checkLpaDependencies() {
+        for (String pkg : getLpaDependencies()) {
+            try {
+                if (!GmsCompat.isGmsApp(pkg, UserHandle.USER_SYSTEM)) {
+                    return false;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
 }
