ce38298cb55c5174fc689643b19a34136c3e3b2b
GrapheneOS <contact@grapheneos.org>
2023-06-06 14:44:05 +0300

    gmscompat: add compatibility layer for unprivileged GMS
    Originally authored by Danny Lin <danny@kdrag0n.dev> for inclusion in
    GrapheneOS. It has since been substantially extended and partially
    rewritten by Dmitry Muhomor <muhomor.dmitry@gmail.com> (primarily) and
    Daniel Micay <danielmicay@gmail.com>.
    
    Commit history:
    (better viewed with git log --grep gmscompat $TAG_NAME)
    
    February 2023 - May 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ2A.230505.002.2023052900
    
    August 2022 - February 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ1A.230205.002.2023020600
    
    June 2022 - August 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.006.2022073000
    
    October 2021 - June 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.002.2022053100
    
    July 2021 - October 2021:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/RQ3A.211001.001.2021100606

---
+++ core/java/android/app/compat/gms/GmsModuleHooks.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2022 GrapheneOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app.compat.gms;
+
+import android.Manifest;
+import android.annotation.NonNull;
+import android.annotation.SystemApi;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Build;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.android.internal.gmscompat.GmsCompatApp;
+import com.android.internal.gmscompat.util.GmsCoreActivityLauncher;
+import com.android.internal.gmscompat.GmsHooks;
+import com.android.internal.gmscompat.StubDef;
+import com.android.internal.gmscompat.util.GmcActivityUtils;
+
+/**
+ * Hooks that are accessed from APEX modules.
+ *
+ * @hide
+ */
+@SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+public class GmsModuleHooks {
+    private static final String TAG = "GmsCompat/MHooks";
+
+    // BluetoothAdapter#enable()
+    // BluetoothAdapter#enableBLE()
+    public static void enableBluetoothAdapter() {
+        Activity activity = GmcActivityUtils.getMostRecentVisibleActivity();
+
+        if (activity != null) {
+            if (GmsCompat.hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                activity.startActivity(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE));
+            } else {
+                String pkgName = GmsCompat.appContext().getPackageName();
+                try {
+                    GmsCompatApp.iGms2Gca().showGmsMissingNearbyDevicesPermissionGeneric(pkgName);
+                } catch (RemoteException e) {
+                    GmsCompatApp.callFailed(e);
+                }
+            }
+        } // else don't bother the user
+    }
+
+    // BluetoothAdapter#setScanMode()
+    public static void makeBluetoothAdapterDiscoverable() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        // Currently used only by Nearby Share.
+        // Ignore other requests, if there are any, to prevent spamming the user with these requests.
+        GmsCoreActivityLauncher.maybeLaunch(intent, ".+nearby\\.sharing.+");
+    }
+
+    // com.android.modules.utils.SynchronousResultReceiver.Result#getValue()
+    public static boolean interceptSynchronousResultReceiverException(@NonNull RuntimeException origException) {
+        if (!(origException instanceof SecurityException)) {
+            return false;
+        }
+
+        // origException contains service-side stack trace, need to obtain an app-side one
+        var stackTrace = new Throwable();
+        StubDef stub = StubDef.find(stackTrace.getStackTrace(), GmsHooks.config(), StubDef.FIND_MODE_SynchronousResultReceiver);
+
+        if (stub == null) {
+            return false;
+        }
+
+        if (stub.type != StubDef.DEFAULT) {
+            Log.d(TAG, "interceptSynchronousResultReceiverException: unexpected stub type " + stub.type, stackTrace);
+            return false;
+        }
+
+        if (Build.isDebuggable()) {
+            Log.i(TAG, "intercepted " + origException, stackTrace);
+        }
+
+        return true;
+    }
+
+    private GmsModuleHooks() {}
+}
