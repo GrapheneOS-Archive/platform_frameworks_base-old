eaff910c173991491d809955a86caca804ec1155
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-19 13:46:06 +0300

    improve compat with apps that try to access network location provider
    If there's no network location provider:
    - redirect network location requests to one of available location providers
    - stub out mock location management calls for network provider (TestProvider methods), since
    redirecting them is infeasible
    
    These changes apply only to third-party apps.
    
    There's a relevant system feature (android.hardware.location.network) that is still listed as
    available to improve app compatibility.
    Removing it would not make this change redundant, because there are apps that try to use the network
    location provider without declaring the need for this feature or checking its presence at runtime.

--- location/java/android/location/HookedLocationManager.java
+++ location/java/android/location/HookedLocationManager.java
@@ -15,10 +15,18 @@ import java.util.function.Consumer;
 
 /** @hide */
 public class HookedLocationManager extends LocationManager {
+
+    private static final int FLAG_ENABLE_PROVIDER_TRANSLATION = 1;
+
     private static int flags;
 
     public static int getFlags(GosPackageStateBase gosPs, boolean isUserApp) {
-        return 0;
+        if (isUserApp) {
+            // see comment in translateProvider()
+            return FLAG_ENABLE_PROVIDER_TRANSLATION;
+        } else {
+            return 0;
+        }
     }
 
     public static void setFlags(int v) {
@@ -33,14 +41,51 @@ public class HookedLocationManager extends LocationManager {
         super(context, service);
     }
 
+    private String translateProvider(String provider) {
+        if ((flags & FLAG_ENABLE_PROVIDER_TRANSLATION) == 0) {
+            return provider;
+        }
+
+        // There are apps that try to use NETWORK_PROVIDER even when it's not present in the list
+        // returned by getAllProviders(), which leads to a crash.
+        // As a workaround, redirect NETWORK_PROVIDER calls to an available provider
+        if (NETWORK_PROVIDER.equals(provider)) {
+            // getAllProviders() list is not guaranteed to be static, don't cache it
+            List<String> allProviders = getAllProviders();
+            if (allProviders.contains(NETWORK_PROVIDER)) {
+                return NETWORK_PROVIDER;
+            }
+            if (allProviders.contains(GPS_PROVIDER)) {
+                return GPS_PROVIDER;
+            }
+            if (allProviders.contains(FUSED_PROVIDER)) {
+                return FUSED_PROVIDER;
+            }
+            // PASSIVE_PROVIDER is always present
+            return PASSIVE_PROVIDER;
+        }
+        return provider;
+    }
+
+    private boolean isMissingProvider(String provider) {
+        // getAllProviders() list is not guaranteed to be static, don't cache it
+        return !getAllProviders().contains(provider);
+    }
+
     @Override
     public boolean isProviderEnabled(@NonNull String provider) {
+        if (isMissingProvider(provider)) {
+            return false;
+        }
+
         return super.isProviderEnabled(provider);
     }
 
     @Nullable
     @Override
     public ProviderProperties getProviderProperties(@NonNull String provider) {
+        provider = translateProvider(provider);
+
         return super.getProviderProperties(provider);
     }
 
@@ -48,6 +93,8 @@ public class HookedLocationManager extends LocationManager {
     @Override
     public Location getLastKnownLocation(@NonNull String provider,
                                          @NonNull LastLocationRequest lastLocationRequest) {
+        provider = translateProvider(provider);
+
         return super.getLastKnownLocation(provider, lastLocationRequest);
     }
 
@@ -55,49 +102,78 @@ public class HookedLocationManager extends LocationManager {
     public void getCurrentLocation(@NonNull String provider, @NonNull LocationRequest locationRequest,
                                    @Nullable CancellationSignal cancellationSignal,
                                    @NonNull Executor executor, @NonNull Consumer<Location> consumer) {
+        provider = translateProvider(provider);
+
         super.getCurrentLocation(provider, locationRequest, cancellationSignal, executor, consumer);
     }
 
     @Override
     public void requestLocationUpdates(@NonNull String provider, @NonNull LocationRequest locationRequest,
                                        @NonNull Executor executor, @NonNull LocationListener listener) {
+        provider = translateProvider(provider);
+
         super.requestLocationUpdates(provider, locationRequest, executor, listener);
     }
 
     @Override
     public void requestLocationUpdates(@NonNull String provider, @NonNull LocationRequest locationRequest,
                                        @NonNull PendingIntent pendingIntent) {
+        provider = translateProvider(provider);
+
         super.requestLocationUpdates(provider, locationRequest, pendingIntent);
     }
 
     @Override
     public void requestFlush(@NonNull String provider, @NonNull LocationListener listener, int requestCode) {
+        provider = translateProvider(provider);
+
         super.requestFlush(provider, listener, requestCode);
     }
 
     @Override
     public void requestFlush(@NonNull String provider, @NonNull PendingIntent pendingIntent, int requestCode) {
+        provider = translateProvider(provider);
+
         super.requestFlush(provider, pendingIntent, requestCode);
     }
+
+
+    // Test providers do not work properly for missing underlying providers, stub them out
 
     @Override
     public void addTestProvider(@NonNull String provider, @NonNull ProviderProperties properties,
                                 @NonNull Set<String> extraAttributionTags) {
+        if (isMissingProvider(provider)) {
+            return;
+        }
+
         super.addTestProvider(provider, properties, extraAttributionTags);
     }
 
     @Override
     public void removeTestProvider(@NonNull String provider) {
+        if (isMissingProvider(provider)) {
+            return;
+        }
+
         super.removeTestProvider(provider);
     }
 
     @Override
     public void setTestProviderEnabled(@NonNull String provider, boolean enabled) {
+        if (isMissingProvider(provider)) {
+            return;
+        }
+
         super.setTestProviderEnabled(provider, enabled);
     }
 
     @Override
     public void setTestProviderLocation(@NonNull String provider, @NonNull Location location) {
+        if (isMissingProvider(provider)) {
+            return;
+        }
+
         super.setTestProviderLocation(provider, location);
     }
 }
