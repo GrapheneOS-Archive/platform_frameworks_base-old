086e01e1f656b4bfdc59a25164889c0255e3cb60
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-23 15:39:53 +0300

    gmscompat: improve compat UI for Nearby Share
    - show notification about missing Nearby devices permission at a predictable time, when
    share/receive activity gets resumed
    - don't convert the "enable Bluetooth" and "make Bluetooth adapter discoverable" requests to
    unprivileged prompts. They are often made by GmsCore at a wrong time or too frequently.
    Instead, show these prompts when share/receive activity gets resumed

--- core/java/com/android/internal/gmscompat/util/GmcActivityUtils.java
+++ core/java/com/android/internal/gmscompat/util/GmcActivityUtils.java
@@ -1,11 +1,18 @@
 package com.android.internal.gmscompat.util;
 
+import android.Manifest;
 import android.annotation.Nullable;
 import android.app.Activity;
 import android.app.Application;
 import android.app.compat.gms.GmsCompat;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.SystemClock;
 
+import com.android.internal.gmscompat.GmsCompatApp;
 import com.android.internal.gmscompat.PlayStoreHooks;
 
 public class GmcActivityUtils implements Application.ActivityLifecycleCallbacks {
@@ -24,6 +31,20 @@ public class GmcActivityUtils implements Application.ActivityLifecycleCallbacks 
     @Override
     public void onActivityResumed(Activity activity) {
         mostRecentVisibleActivity = activity;
+
+        String className = activity.getClass().getName();
+
+        if (GmsCompat.isGmsCore()) {
+            switch (className) {
+                case "com.google.android.gms.nearby.sharing.ShareSheetActivity":
+                    handleNearbyShareActivityResume(activity, true);
+                    break;
+                case "com.google.android.gms.nearby.sharing.InternalReceiveSurfaceActivity":
+                    handleNearbyShareActivityResume(activity, false);
+                    break;
+            }
+        }
+
         if (GmsCompat.isPlayStore()) {
             PlayStoreHooks.onActivityResumed(activity);
         }
@@ -41,4 +62,41 @@ public class GmcActivityUtils implements Application.ActivityLifecycleCallbacks 
     @Override public void onActivityStopped(Activity activity) {}
     @Override public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
     @Override public void onActivityDestroyed(Activity activity) {}
+
+    private static long lastBtEnableRequest;
+    private static long lastBtDiscoverabilityRequest;
+
+    private static void handleNearbyShareActivityResume(Activity activity, boolean isSend) {
+        if (GmsCompat.hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+            var bm = GmsCompat.appContext().getSystemService(BluetoothManager.class);
+            BluetoothAdapter adapter = bm.getAdapter();
+
+            final long repeatInterval = 20_000;
+
+            long ts = SystemClock.elapsedRealtime();
+            if (isSend) {
+                if (adapter.getState() == BluetoothAdapter.STATE_OFF) {
+                    if (ts - lastBtEnableRequest > repeatInterval) {
+                        var intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        activity.startActivity(intent);
+                        lastBtEnableRequest = ts;
+                    }
+                }
+            } else {
+                if (adapter.getScanMode() == BluetoothAdapter.SCAN_MODE_NONE) {
+                    if (ts - lastBtDiscoverabilityRequest > repeatInterval) {
+                        var intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                        activity.startActivity(intent);
+                        lastBtDiscoverabilityRequest = ts;
+                    }
+                }
+            }
+        } else {
+            try {
+                GmsCompatApp.iGms2Gca().showGmsCoreMissingPermissionForNearbyShareNotification();
+            } catch (RemoteException e) {
+                GmsCompatApp.callFailed(e);
+            }
+        }
+    }
 }
