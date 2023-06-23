3662487940088b55642026bdfa8f8c444dda5325
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-23 10:49:32 +0300

    SystemUI: apply "Add users from lock screen" setting to guest users

--- packages/SystemUI/src/com/android/systemui/user/domain/interactor/UserInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/user/domain/interactor/UserInteractor.kt
@@ -765,6 +765,10 @@ constructor(
     }
 
     private fun canCreateGuestUser(settings: UserSwitcherSettingsModel): Boolean {
+        if (!settings.isAddUsersFromLockscreen) {
+            return false
+        }
+
         return guestUserInteractor.isGuestUserAutoCreated ||
             UserActionsUtil.canCreateGuest(
                 manager,
