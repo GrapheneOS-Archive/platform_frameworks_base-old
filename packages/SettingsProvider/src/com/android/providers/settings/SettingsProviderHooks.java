23eafdbefed1322108fedc7888eb47642a457f38
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-06-29 12:41:29 +0000

    SettingsProviderHooks: Infrastructure for hooking settings state init

---
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProviderHooks.java
@@ -0,0 +1,22 @@
+package com.android.providers.settings;
+
+import android.os.UserHandle;
+import android.provider.Settings;
+
+class SettingsProviderHooks {
+
+    static void onSettingsStateInit(final SettingsProvider.SettingsRegistry registry, final int userId) {
+        if (userId == UserHandle.USER_SYSTEM) {
+            SettingsState globalSettings = registry.getSettingsLocked(SettingsProvider.SETTINGS_TYPE_GLOBAL, userId);
+        }
+        SettingsState secureSettings = registry.getSettingsLocked(SettingsProvider.SETTINGS_TYPE_SECURE, userId);
+    }
+
+    /**
+     * Unlike UpgradeController#upgradeIfNeededLocked settings migration, this runs every time a user is initialized.
+     * Insert or modify setting upon SettingState initialization for any user, or in case of system user, upon boot.
+     */
+    private static void insertSetting(SettingsState state, String key, String value) {
+        state.insertSettingLocked(key, value, null /* tag */, false /* makeDefault */, SettingsState.SYSTEM_PACKAGE_NAME);
+    }
+}
