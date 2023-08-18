a44445e8fe284d78670fac3f1189bc6fcacf02f4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-08-18 12:21:13 +0300

    add hooks for modifying ActivityThread behavior

---
+++ core/java/android/app/ActivityThreadHooks.java
@@ -0,0 +1,17 @@
+package android.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Process;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.Objects;
+
+class ActivityThreadHooks {
+
+    static Service instantiateService(String className) {
+        Service res = null;
+        return res;
+    }
+}
