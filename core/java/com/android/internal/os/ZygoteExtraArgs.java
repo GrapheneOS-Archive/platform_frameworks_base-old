a75baff97f4623f61974a6fb6696243acffa717d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-24 13:13:13 +0300

    infrastructure for passing SELinux flags to the kernel

--- core/java/com/android/internal/os/ZygoteExtraArgs.java
+++ core/java/com/android/internal/os/ZygoteExtraArgs.java
@@ -12,37 +12,49 @@ import android.content.pm.GosPackageStateBase;
 // AppZygote is treated differently from WebViewZygote because the former runs untrusted app code
 // (see android.app.ZygotePreload).
 public class ZygoteExtraArgs {
+    public final long selinuxFlags;
+
     public static final String PREFIX = "--flat-extra-args=";
 
-    public static final ZygoteExtraArgs DEFAULT = new ZygoteExtraArgs();
+    public static final ZygoteExtraArgs DEFAULT = new ZygoteExtraArgs(0L);
 
-    public ZygoteExtraArgs() {
+    public ZygoteExtraArgs(long selinuxFlags) {
+        this.selinuxFlags = selinuxFlags;
     }
 
-    private static final int ARR_LEN = 0;
+    private static final int IDX_SELINUX_FLAGS = 0;
+    private static final int ARR_LEN = 1;
     private static final String SEPARATOR = "\t";
 
     public static String createFlat(Context ctx, int userId, ApplicationInfo appInfo,
                                 @Nullable GosPackageStateBase ps,
                                 boolean isIsolatedProcess) {
         String[] arr = new String[ARR_LEN];
+        arr[IDX_SELINUX_FLAGS] = Long.toHexString(
+            SELinuxFlags.get(ctx, userId, appInfo, ps, isIsolatedProcess)
+        );
         return PREFIX + String.join(SEPARATOR, arr);
     }
 
     public static String createFlatForWebviewProcess(Context ctx, int userId,
              ApplicationInfo callerAppInfo, @Nullable GosPackageStateBase callerPs) {
         String[] arr = new String[ARR_LEN];
+        arr[IDX_SELINUX_FLAGS] = Long.toHexString(
+            SELinuxFlags.getForWebViewProcess(ctx, userId, callerAppInfo, callerPs)
+        );
         return PREFIX + String.join(SEPARATOR, arr);
     }
 
     static ZygoteExtraArgs parse(String flat) {
         String[] arr = flat.split(SEPARATOR);
-        return new ZygoteExtraArgs();
+        long selinuxFlags = Long.parseLong(arr[IDX_SELINUX_FLAGS], 16);
+        return new ZygoteExtraArgs(selinuxFlags);
     }
 
     // keep in sync with ExtraArgs struct in core/jni/com_android_internal_os_Zygote.cpp
     public long[] makeJniLongArray() {
         long[] res = new long[ARR_LEN];
+        res[IDX_SELINUX_FLAGS] = selinuxFlags;
         return res;
     }
 }
