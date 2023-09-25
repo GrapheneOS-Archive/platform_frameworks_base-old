38d77c44cd6b16b9f31440ce72bf8274587135a7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-25 11:23:55 +0300

    extsettings: support using resource reference for default setting value

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -1,10 +1,20 @@
 package android.ext.settings;
 
+import android.annotation.BoolRes;
+import android.annotation.IntegerRes;
+import android.annotation.StringRes;
+import android.app.AppGlobals;
 import android.content.Context;
+import android.content.res.Resources;
+
+import com.android.internal.R;
 
 import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BooleanSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
 
 import static android.ext.settings.GnssConstants.PSDS_DISABLED;
 import static android.ext.settings.GnssConstants.PSDS_SERVER_GRAPHENEOS;
@@ -146,5 +156,21 @@ public class ExtSettings {
                 }
             }
         }
+    }
+
+    public static BooleanSupplier defaultBool(@BoolRes int res) {
+        return () -> getResources().getBoolean(res);
+    }
+
+    public static IntSupplier defaultInt(@IntegerRes int res) {
+        return () -> getResources().getInteger(res);
+    }
+
+    public static Supplier<String> defaultString(@StringRes int res) {
+        return () -> getResources().getString(res);
+    }
+
+    public static Resources getResources() {
+        return AppGlobals.getInitialApplication().getResources();
     }
 }
