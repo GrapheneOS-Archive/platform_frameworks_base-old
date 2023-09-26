916046a6487fe29d1620aca668eeda6e1d44f06a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-26 11:07:51 +0300

    add per-app native debugging setting
    Value of the previous global setting is used as a default value of the new per-app setting.
    
    For system apps, native debugging is blocked unconditionally.

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -132,6 +132,9 @@ public class ExtSettings {
         }
     };
 
+    public static final BoolSysProperty ALLOW_NATIVE_DEBUG_BY_DEFAULT = new BoolSysProperty(
+            "persist.native_debug", defaultBool(R.bool.setting_default_allow_native_debugging));
+
     // AppCompatConfig specifies which hardening features are compatible/incompatible with a
     // specific app.
     // This setting controls whether incompatible hardening features would be disabled by default
