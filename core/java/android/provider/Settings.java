1ea68d5769b222056f452a4b326aa6c57ad89208
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-26 11:07:57 +0300

    add per-app dynamic code execution settings
    - dynamic code execution from memory is blocked for all system apps except Vanadium
    - dynamic code execution from storage is blocked for all system apps
    - dynamic code execution in WebView (i.e. Vanadium JIT) is currently not restricted due to the lack
    of support in Vanadium WebView.
    - UI for these restrictions is not yet implemented

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12637,6 +12637,15 @@ public final class Settings {
         public static final String REMOTE_KEY_PROVISIONING_SERVER = "attest_remote_provisioner_server";
 
         /** @hide */
+        public static final String RESTRICT_MEMORY_DYN_CODE_LOADING_BY_DEFAULT = "restrict_memory_dyn_code_exec";
+
+        /** @hide */
+        public static final String RESTRICT_STORAGE_DYN_CODE_LOADING_BY_DEFAULT = "restrict_storage_dyn_code_exec";
+
+        /** @hide */
+        public static final String RESTRICT_WEBVIEW_DYN_CODE_LOADING_BY_DEFAULT = "restrict_webview_dyn_code_exec";
+
+        /** @hide */
         public static final String SHOW_SYSTEM_PROCESS_CRASH_NOTIFICATIONS = "show_system_process_crash_notifs";
 
         // ExtSettings END
