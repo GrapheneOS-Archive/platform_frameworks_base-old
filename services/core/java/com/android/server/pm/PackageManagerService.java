ca87c73950da014aa79c8fc86ab6131f06328916
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-03 11:25:07 +0300

    support showing dexopt progress during boot
    showDexoptProgressBootMessage() is called by libartservice in art repo during post-OTA dexopt.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -7905,4 +7905,9 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         // use the same procedure that is used for SELinux compat changes
         selinuxChangeListener.onCompatChange(packageName);
     }
+
+    @NonNull
+    public Context getContext() {
+        return mContext;
+    }
 }
