c172f16eeac905c498acb8afc8ed5b5e7c08a555
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-07 14:49:26 +0300

    PackageSpec: add an extra utility method

--- services/core/java/com/android/server/ext/PackageManagerUtils.java
+++ services/core/java/com/android/server/ext/PackageManagerUtils.java
@@ -1,9 +1,21 @@
 package com.android.server.ext;
 
+import android.content.pm.PackageManagerInternal;
+
 import com.android.internal.util.PackageSpec;
+import com.android.server.LocalServices;
 import com.android.server.pm.pkg.AndroidPackage;
 
 public class PackageManagerUtils {
+
+    public static boolean validatePackage(PackageSpec spec) {
+        var pm = LocalServices.getService(PackageManagerInternal.class);
+        AndroidPackage pkg = pm.getPackage(spec.packageName);
+        if (pkg != null) {
+            return validatePackage(pkg, spec);
+        }
+        return false;
+    }
 
     public static boolean validatePackage(AndroidPackage pkg, PackageSpec spec) {
         // PackageSpec can't reference AndroidPackage directly, it's compiled separately
