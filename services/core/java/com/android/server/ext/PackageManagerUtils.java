213382b80cbac8e4f89e9d15fce82f1704efa93b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-11 12:56:06 +0300

    PackageSpec: make validator into an interface
    This allows to simplify usage of separate validators in system_server and non-system_server code.

--- services/core/java/com/android/server/ext/PackageManagerUtils.java
+++ services/core/java/com/android/server/ext/PackageManagerUtils.java
@@ -8,6 +8,21 @@ import com.android.server.pm.pkg.AndroidPackage;
 
 public class PackageManagerUtils {
 
+    public static PackageSpec.Validator getPackageSpecValidator() {
+        var pm = LocalServices.getService(PackageManagerInternal.class);
+        return getPackageSpecValidator(pm);
+    }
+
+    public static PackageSpec.Validator getPackageSpecValidator(PackageManagerInternal pm) {
+        return (PackageSpec spec) -> {
+            AndroidPackage pkg = pm.getPackage(spec.packageName);
+            if (pkg != null) {
+                return validatePackage(pkg, spec);
+            }
+            return false;
+        };
+    }
+
     public static boolean validatePackage(PackageSpec spec) {
         var pm = LocalServices.getService(PackageManagerInternal.class);
         AndroidPackage pkg = pm.getPackage(spec.packageName);
