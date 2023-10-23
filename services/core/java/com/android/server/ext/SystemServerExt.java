56115ecfda66c5ffba299c6d39fca0b867e1ae40
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-23 14:57:47 +0300

    remove error-prone SystemServerExt.get() method
    SystemServerExt is initialized late. To avoid races, it's better to pass references to it explicitly.

--- services/core/java/com/android/server/ext/SystemServerExt.java
+++ services/core/java/com/android/server/ext/SystemServerExt.java
@@ -1,24 +1,6 @@
-/*
- * Copyright (C) 2022 GrapheneOS
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.server.ext;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 
@@ -30,12 +12,6 @@ public final class SystemServerExt {
     public final Context context;
     public final Handler bgHandler;
     public final PackageManagerService packageManager;
-
-    private static SystemServerExt instance;
-
-    public static SystemServerExt get() {
-        return instance;
-    }
 
     private SystemServerExt(Context systemContext, PackageManagerService pm) {
         context = systemContext;
@@ -53,7 +29,6 @@ public final class SystemServerExt {
     public static void init(Context systemContext, PackageManagerService pm) {
         SystemServerExt sse = new SystemServerExt(systemContext, pm);
         sse.bgHandler.post(sse::initBgThread);
-        instance = sse;
 
         new GoogleEuiccLpaDisabler(sse);
     }
