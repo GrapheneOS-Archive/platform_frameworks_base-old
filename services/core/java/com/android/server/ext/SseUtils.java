dfad479e8ea257f6758cd5e53c980d45b7a4cefc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-23 14:57:14 +0300

    SseUtils: add a method for adding Notification.Action

--- services/core/java/com/android/server/ext/SseUtils.java
+++ services/core/java/com/android/server/ext/SseUtils.java
@@ -2,6 +2,7 @@ package com.android.server.ext;
 
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 
 public class SseUtils {
@@ -14,5 +15,10 @@ public class SseUtils {
         var ctx = SystemServerExt.get().context;
         var pi = PendingIntent.getBroadcast(ctx, 0, broadcastIntent, PendingIntent.FLAG_IMMUTABLE);
         return new Notification.Action.Builder(null, ctx.getText(textRes), pi);
+    }
+
+    public static void addNotifAction(Context ctx, PendingIntent intent, int textRes, Notification.Builder dst) {
+        var nb = new Notification.Action.Builder(null, ctx.getText(textRes), intent);
+        dst.addAction(nb.build());
     }
 }
