f0ed79d8a91b7e66f7466f45afac0a249d870e90
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-08 08:53:43 +0200

    add system API for getting app info by PID

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19053,6 +19053,11 @@ public class ActivityManagerService extends IActivityManager.Stub
                 mProcessList.onGosPackageStateChangedLOSP(uid, state);
             }
         }
+
+        @Override
+        public ProcessRecordSnapshot getProcessRecordByPid(int pid) {
+            return mProcessList.getProcessRecordByPid(pid);
+        }
     }
 
     long inputDispatchingTimedOut(int pid, final boolean aboveSystem, TimeoutRecord timeoutRecord) {
