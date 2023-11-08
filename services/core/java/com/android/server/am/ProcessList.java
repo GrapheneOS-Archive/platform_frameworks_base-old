1f0a5be376a8161fb0000e759e537b12d76e8ffc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-08 08:53:55 +0200

    always stop app zygotes when app is being force-stopped
    Some of per-app features (e.g. SELinux restrictions) require app process restart to be applied.

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -3135,7 +3135,7 @@ public final class ProcessList {
                 removeProcessLocked(proc.first, callerWillRestart, allowRestart || proc.second,
                         reasonCode, subReason, reason, !doFreeze /* async */);
             }
-            killAppZygotesLocked(packageName, appId, userId, false /* force */);
+            killAppZygotesLocked(packageName, appId, userId, true /* force */);
 
             if (doFreeze) unfreezePackageCgroup(packageUID);
 
