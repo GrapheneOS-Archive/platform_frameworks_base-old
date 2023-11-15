3459a61a78c0efe5749ba3b8a69c528ca2eacaf2
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-15 17:26:29 +0200

    add constants for some system package names

---
+++ core/java/android/ext/KnownSystemPackages.java
@@ -0,0 +1,8 @@
+package android.ext;
+
+/** @hide */
+public interface KnownSystemPackages {
+    String PERMISSION_CONTROLLER = "com.android.permissioncontroller";
+    String SETTINGS = "com.android.settings";
+    String SYSTEM_UI = "com.android.systemui";
+}
