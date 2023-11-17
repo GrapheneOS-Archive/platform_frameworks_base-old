ccff020519818ed27b99cca28a9bf1812c17d175
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-17 09:02:30 +0200

    disable hardened_malloc by default for Pixel Camera

--- core/java/android/ext/compat/ExtAppCompat.java
+++ core/java/android/ext/compat/ExtAppCompat.java
@@ -6,8 +6,10 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.ext.settings.app.AswUseHardenedMalloc;
 import android.os.Binder;
 
+import com.android.internal.util.GoogleCameraUtils;
 import com.android.internal.util.PackageSpec;
 
 /** @hide */
@@ -16,6 +18,13 @@ public class ExtAppCompat {
     @Nullable
     public static AppCompatConfig getAppCompatConfig(ApplicationInfo appInfo, int userId) {
         switch (appInfo.packageName) {
+            case GoogleCameraUtils.PACKAGE_NAME -> {
+                if (validatePackageSpec(GoogleCameraUtils.PACKAGE_SPEC, userId)) {
+                    var c = new AppCompatConfig(GoogleCameraUtils.PACKAGE_SPEC);
+                    c.incompatibleWith(AswUseHardenedMalloc.class);
+                    return c;
+                }
+            }
         }
 
         return null;
