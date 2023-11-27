66aef883b72b93bd6b196b73ebfb49592cb8fda4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-27 20:40:28 +0200

    infrastructure for attaching extra data to ApplicationInfo objects

--- core/java/android/content/pm/ApplicationInfo.java
+++ core/java/android/content/pm/ApplicationInfo.java
@@ -29,6 +29,7 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
+import android.ext.AppInfoExt;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Environment;
@@ -1901,6 +1902,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
 
     public ApplicationInfo(ApplicationInfo orig) {
         super(orig);
+        ext = orig.ext;
         taskAffinity = orig.taskAffinity;
         permission = orig.permission;
         mKnownActivityEmbeddingCerts = orig.mKnownActivityEmbeddingCerts;
@@ -1989,6 +1991,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
             return;
         }
         super.writeToParcel(dest, parcelableFlags);
+        ext.writeToParcel(dest, parcelableFlags);
         dest.writeString8(taskAffinity);
         dest.writeString8(permission);
         dest.writeString8(processName);
@@ -2092,6 +2095,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
     @SuppressWarnings("unchecked")
     private ApplicationInfo(Parcel source) {
         super(source);
+        ext = AppInfoExt.CREATOR.createFromParcel(source);
         taskAffinity = source.readString8();
         permission = source.readString8();
         processName = source.readString8();
@@ -2806,5 +2810,18 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
         } else {
             privateFlagsExt &= ~PRIVATE_FLAG_EXT_ENABLE_ON_BACK_INVOKED_CALLBACK;
         }
+    }
+
+    private AppInfoExt ext = AppInfoExt.DEFAULT;
+
+    /** @hide */
+    public void setExt(AppInfoExt ext) {
+        this.ext = ext;
+    }
+
+    /** @hide */
+    @SystemApi
+    public @NonNull AppInfoExt ext() {
+        return ext;
     }
 }
