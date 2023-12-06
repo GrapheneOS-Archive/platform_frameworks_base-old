10a3498fe04a2c8460dfbc4044250600da152040
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-07 01:47:04 +0200

    fixup! automatically reboot device after timeout if set

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1859,7 +1859,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
     }
 
     private void doRebootForOwnerAfterTimeoutIfEnabled(long rebootAfterTimeout) {
-        long when = SystemClock.elapsedRealtime() + rebootAfterTimeout;
+        long when = mSystemClock.elapsedRealtime() + rebootAfterTimeout;
         Intent rebootIntent = new Intent(DELAYED_REBOOT_ACTION);
         rebootIntent.putExtra("seq", mDelayedRebootSequence);
         rebootIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
