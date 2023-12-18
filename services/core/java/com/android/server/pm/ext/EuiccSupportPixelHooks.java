82654c9fd6172b6dd00f829ab07568628a4c7d52
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 16:15:48 +0200

    switch EuiccSupportPixel integration to PackageHooks

---
+++ services/core/java/com/android/server/pm/ext/EuiccSupportPixelHooks.java
@@ -0,0 +1,24 @@
+package com.android.server.pm.ext;
+
+import android.Manifest;
+
+import com.android.server.pm.pkg.PackageStateInternal;
+import com.android.server.pm.pkg.component.ParsedUsesPermission;
+
+class EuiccSupportPixelHooks extends PackageHooks {
+
+    static class ParsingHooks extends PackageParsingHooks {
+        @Override
+        public boolean shouldSkipUsesPermission(ParsedUsesPermission p) {
+            // EuiccSupportPixel uses INTERNET perm only as part of its dev mode
+            return Manifest.permission.INTERNET.equals(p.getName());
+        }
+    }
+
+    @Override
+    public boolean shouldBlockPackageVisibility(int userId, PackageStateInternal otherPkg) {
+        // EuiccSupportPixel is a privileged package, block it from interacting with unprivileged
+        // parts of GMS
+        return isUserInstalledPkg(otherPkg);
+    }
+}
