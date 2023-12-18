577bb15ed6a8d0c2cc8c778ca56d375018a015d3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 16:12:35 +0200

    gmscompat: switch GmsCompat to PackageHooks

---
+++ services/core/java/com/android/server/pm/ext/GmsCompatPkgParsingHooks.java
@@ -0,0 +1,61 @@
+package com.android.server.pm.ext;
+
+import android.Manifest;
+import android.annotation.CallSuper;
+import android.annotation.Nullable;
+import android.app.compat.gms.GmsCompat;
+import android.content.pm.ServiceInfo;
+import android.ext.PackageId;
+import android.os.Bundle;
+
+import com.android.internal.gmscompat.client.GmsCompatClientService;
+import com.android.server.pm.pkg.component.ParsedService;
+import com.android.server.pm.pkg.component.ParsedServiceImpl;
+import com.android.server.pm.pkg.component.ParsedUsesPermissionImpl;
+import com.android.server.pm.pkg.parsing.ParsingPackage;
+
+import java.util.List;
+
+public class GmsCompatPkgParsingHooks extends PackageParsingHooks {
+
+    @Nullable
+    public static PackageParsingHooks maybeGet(String pkgName) {
+        if (!GmsCompat.canBeEnabledFor(pkgName)) {
+            return null;
+        }
+
+        return switch (pkgName) {
+            case PackageId.GSF_NAME -> new GsfParsingHooks();
+            case PackageId.GMS_CORE_NAME -> new GmsCoreHooks.ParsingHooks();
+            case PackageId.PLAY_STORE_NAME -> new PlayStoreHooks.ParsingHooks();
+            case PackageId.G_CARRIER_SETTINGS_NAME -> new GCarrierSettingsHooks.ParsingHooks();
+            default -> new GmsCompatPkgParsingHooks();
+        };
+    }
+
+    public static ParsedService maybeCreateClientService(ParsingPackage pkg) {
+        Bundle metadata = pkg.getMetaData();
+        boolean isGmsCoreClient = metadata != null && metadata.containsKey("com.google.android.gms.version");
+
+        if (!isGmsCoreClient || PackageId.GMS_CORE_NAME.equals(pkg.getPackageName())) {
+            return null;
+        }
+
+        return createService(pkg, GmsCompatClientService.class.getName());
+    }
+
+    @CallSuper
+    @Override
+    public List<ParsedUsesPermissionImpl> addUsesPermissions() {
+        return createUsesPerms(Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE);
+    }
+
+    @CallSuper
+    @Override
+    public void amendParsedService(ParsedServiceImpl s) {
+        if (s.getForegroundServiceType() == ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED) {
+            // FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED requires special privileges
+            s.setForegroundServiceType(ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+        }
+    }
+}
--- services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
+++ services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
@@ -6,6 +6,11 @@ import android.ext.PackageId;
 public class PackageHooksRegistry {
 
     public static PackageParsingHooks getParsingHooks(String pkgName) {
+        PackageParsingHooks gmsCompatHooks = GmsCompatPkgParsingHooks.maybeGet(pkgName);
+        if (gmsCompatHooks != null) {
+            return gmsCompatHooks;
+        }
+
         return switch (pkgName) {
             case PackageId.EUICC_SUPPORT_PIXEL_NAME -> new EuiccSupportPixelHooks.ParsingHooks();
             default -> PackageParsingHooks.DEFAULT;
@@ -15,6 +20,7 @@ public class PackageHooksRegistry {
     public static PackageHooks getHooks(int packageId) {
         return switch (packageId) {
             case PackageId.EUICC_SUPPORT_PIXEL -> new EuiccSupportPixelHooks();
+            case PackageId.G_CARRIER_SETTINGS -> new GCarrierSettingsHooks();
             default -> PackageHooks.DEFAULT;
         };
     }
