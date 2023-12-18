20ac63a216e344e1a4af43690d6061ccfd969939
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 19:28:01 +0200

    PackageHooks: infrastructure for per-package hooks in system_server

--- services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
+++ services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
@@ -10,4 +10,10 @@ public class PackageHooksRegistry {
             default -> PackageParsingHooks.DEFAULT;
         };
     }
+
+    public static PackageHooks getHooks(int packageId) {
+        return switch (packageId) {
+            default -> PackageHooks.DEFAULT;
+        };
+    }
 }
