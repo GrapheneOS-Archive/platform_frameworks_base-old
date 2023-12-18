64241008e4dbe97083fbb230e932567699ec0ccd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 17:11:09 +0200

    gmscompat: improve initialization
    - use PackageIds to remove the need to fetch and check certificates in app processes
    - rename isGmsApp() to isEnabledFor()

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -13470,7 +13470,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                 if (ActivityManager.checkUidPermission(
                         INTERACT_ACROSS_USERS,
                         aInfo.uid) != PackageManager.PERMISSION_GRANTED) {
-                    if (GmsCompat.isGmsApp(aInfo)) {
+                    if (GmsCompat.isEnabledFor(aInfo)) {
                         return false;
                     }
 
