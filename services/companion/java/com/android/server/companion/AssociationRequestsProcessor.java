0df5c4fe5cdb9c673da629b59951a6fcec3971ff
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 19:28:43 +0200

    aauto: support allowing association with "automotive projection" devices

--- services/companion/java/com/android/server/companion/AssociationRequestsProcessor.java
+++ services/companion/java/com/android/server/companion/AssociationRequestsProcessor.java
@@ -32,6 +32,7 @@ import static com.android.server.companion.utils.RolesUtils.addRoleHolderForAsso
 import static com.android.server.companion.utils.RolesUtils.isRoleHolder;
 import static com.android.server.companion.utils.Utils.prepareForIpc;
 
+import static com.android.server.pm.ext.AndroidAutoHooks.isAndroidAutoWithGrantedBasePrivPerms;
 import static java.util.Objects.requireNonNull;
 
 import android.annotation.NonNull;
@@ -171,10 +172,14 @@ class AssociationRequestsProcessor {
         enforcePermissionForCreatingAssociation(mContext, request, packageUid);
         enforceUsesCompanionDeviceFeature(mContext, userId, packageName);
 
+        final boolean shouldSkipAddRoleHolderCheck =
+                AssociationRequest.DEVICE_PROFILE_AUTOMOTIVE_PROJECTION.equals(request.getDeviceProfile())
+                        && isAndroidAutoWithGrantedBasePrivPerms(packageName, userId);
+
         // 2a. Check if association can be created without launching UI (i.e. CDM needs NEITHER
         // to perform discovery NOR to collect user consent).
         if (request.isSelfManaged() && !request.isForceConfirmation()
-                && !willAddRoleHolder(request, packageName, userId)) {
+                && (shouldSkipAddRoleHolderCheck || !willAddRoleHolder(request, packageName, userId))) {
             // 2a.1. Create association right away.
             createAssociationAndNotifyApplication(request, packageName, userId,
                     /* macAddress */ null, callback, /* resultReceiver */ null);
@@ -301,8 +306,17 @@ class AssociationRequestsProcessor {
                 selfManaged, /* notifyOnDeviceNearby */ false, /* revoked */ false,
                 /* pending */ false, timestamp, Long.MAX_VALUE, /* systemDataSyncFlags */ 0);
 
-        // Add role holder for association (if specified) and add new association to store.
-        maybeGrantRoleAndStoreAssociation(association, callback, resultReceiver);
+        final boolean skipAddRoleHolder =
+                AssociationRequest.DEVICE_PROFILE_AUTOMOTIVE_PROJECTION.equals(deviceProfile)
+                        && isAndroidAutoWithGrantedBasePrivPerms(packageName, userId);
+
+        if (skipAddRoleHolder) {
+            addAssociationToStore(association);
+            sendCallbackAndFinish(association, callback, resultReceiver);
+        } else {
+            // Add role holder for association (if specified) and add new association to store.
+            maybeGrantRoleAndStoreAssociation(association, callback, resultReceiver);
+        }
 
         // Don't need to update the mRevokedAssociationsPendingRoleHolderRemoval since
         // maybeRemoveRoleHolderForAssociation in PackageInactivityListener will handle the case
