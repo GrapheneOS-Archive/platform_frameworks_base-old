90fbca5c46b8d8fbe205deddd1ff3861a9bc9d2f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 16:18:30 +0200

    PackageExt: infrastructure for attaching extra data to known parsed APKs

--- services/core/java/com/android/server/pm/parsing/pkg/PackageImpl.java
+++ services/core/java/com/android/server/pm/parsing/pkg/PackageImpl.java
@@ -3242,6 +3242,7 @@ public class PackageImpl implements ParsedPackage, AndroidPackageInternal,
         dest.writeInt(this.uid);
         dest.writeLong(this.mBooleans);
         dest.writeLong(this.mBooleans2);
+        this.ext.writeToParcel(dest);
     }
 
     public PackageImpl(Parcel in) {
@@ -3404,6 +3405,8 @@ public class PackageImpl implements ParsedPackage, AndroidPackageInternal,
 
         assignDerivedFields();
         assignDerivedFields2();
+
+        this.ext = com.android.server.pm.ext.PackageExt.createFromParcel(this, in);
 
         // Do not call makeImmutable here as cached parsing will need
         // to mutate this instance before it's finalized.
@@ -3788,5 +3791,18 @@ public class PackageImpl implements ParsedPackage, AndroidPackageInternal,
     @Override
     public com.android.server.pm.ext.PackageParsingHooks getPackageParsingHooks() {
         return packageParsingHooks;
+    }
+
+    private com.android.server.pm.ext.PackageExt ext = com.android.server.pm.ext.PackageExt.DEFAULT;
+
+    @Override
+    public void setPackageExt(@Nullable com.android.server.pm.ext.PackageExt ext) {
+        this.ext = ext;
+    }
+
+    @Nullable
+    @Override
+    public com.android.server.pm.ext.PackageExt ext() {
+        return ext;
     }
 }
--- services/core/java/com/android/server/pm/pkg/parsing/ParsingPackage.java
+++ services/core/java/com/android/server/pm/pkg/parsing/ParsingPackage.java
@@ -540,4 +540,6 @@ public interface ParsingPackage {
     void initPackageParsingHooks();
 
     com.android.server.pm.ext.PackageParsingHooks getPackageParsingHooks();
+
+    void setPackageExt(@Nullable com.android.server.pm.ext.PackageExt ext);
 }
