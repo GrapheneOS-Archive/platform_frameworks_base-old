14549fdd73bc7b217d814034f361d5a2f0581727
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 19:38:05 +0200

    cache AppCompatConfig values inside parsed APKs that it applies to
    Searching for AppCompatConfig each time ApplicationInfo is constructed is inefficient.

--- services/core/java/com/android/server/pm/parsing/pkg/PackageImpl.java
+++ services/core/java/com/android/server/pm/parsing/pkg/PackageImpl.java
@@ -3800,4 +3800,31 @@ public class PackageImpl implements ParsedPackage, AndroidPackageInternal,
     public com.android.server.pm.ext.PackageExt ext() {
         return ext;
     }
+
+    private long cachedCompatConfigVersionCode;
+    private com.android.server.os.nano.AppCompatProtos.CompatConfig cachedCompatConfig;
+
+    @Nullable
+    public com.android.server.os.nano.AppCompatProtos.CompatConfig getAppCompatConfig() {
+        var configs = com.android.server.ext.AppCompatConf.getParsedConfigs();
+
+        if (configs == null) {
+            return null;
+        }
+
+        synchronized (this) {
+            if (configs.versionCode == cachedCompatConfigVersionCode) {
+                return cachedCompatConfig;
+            }
+        }
+
+        var config = com.android.server.ext.AppCompatConf.get(configs, this);
+
+        synchronized (this) {
+            cachedCompatConfigVersionCode = configs.versionCode;
+            cachedCompatConfig = config;
+        }
+
+        return config;
+    }
 }
