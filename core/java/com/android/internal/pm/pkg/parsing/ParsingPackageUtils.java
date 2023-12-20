0be46e0529c25d56e6143313c111f1414d60fa7d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-20 13:04:44 +0200

    infrastructure for custom handling of known packages

--- core/java/com/android/internal/pm/pkg/parsing/ParsingPackageUtils.java
+++ core/java/com/android/internal/pm/pkg/parsing/ParsingPackageUtils.java
@@ -144,6 +144,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 
 /**
  * TODO(b/135203078): Differentiate between parse_ methods and some add_ method for whether it
@@ -629,9 +630,22 @@ public class ParsingPackageUtils {
 
             pkg.setVolumeUuid(volumeUuid);
 
+            PackageExtInitIface pkgExtInit = null;
+            PackageExtInitSupplier pkgExtInitSupplier = packageExtInitSupplier;
+            if (pkgExtInitSupplier != null) {
+                pkgExtInit = pkgExtInitSupplier.invoke(input, pkg, (flags & PARSE_IS_SYSTEM_DIR) != 0);
+                if (pkgExtInit != null) {
+                    pkgExtInit.run();
+                }
+            }
+
             if ((flags & PARSE_COLLECT_CERTIFICATES) != 0) {
-                final ParseResult<SigningDetails> ret =
-                        getSigningDetails(input, pkg, false /*skipVerify*/);
+                // skip reparsing certificates if they were already parsed by PackageExtInit
+                ParseResult<SigningDetails> ret = pkgExtInit != null ?
+                        pkgExtInit.getSigningDetailsParseResult() : null;
+                if (ret == null) {
+                    ret = parseSigningDetails(input, pkg);
+                }
                 if (ret.isError()) {
                     return input.error(ret);
                 }
@@ -651,6 +665,22 @@ public class ParsingPackageUtils {
             return input.error(INSTALL_PARSE_FAILED_UNEXPECTED_EXCEPTION,
                     "Failed to read manifest from " + apkPath, e);
         }
+    }
+
+    public interface PackageExtInitSupplier {
+        PackageExtInitIface invoke(ParseInput input, ParsingPackage pkg, boolean isSystem);
+    }
+
+    @Nullable
+    public static PackageExtInitSupplier packageExtInitSupplier;
+
+    public interface PackageExtInitIface {
+        void run();
+        ParseResult<SigningDetails> getSigningDetailsParseResult();
+    }
+
+    public static ParseResult<SigningDetails> parseSigningDetails(ParseInput input, ParsingPackage pkg) {
+        return getSigningDetails(input, pkg, false /*skipVerify*/);
     }
 
     private ParseResult<ParsingPackage> parseSplitApk(ParseInput input,
@@ -1038,6 +1068,27 @@ public class ParsingPackageUtils {
             );
         }
 
+        List<ParsedUsesPermission> usesPermsList = pkg.getUsesPermissions();
+        var usesPerms = new java.util.HashSet<String>(usesPermsList.size() + 10);
+        for (ParsedUsesPermission p : usesPermsList) {
+            usesPerms.add(p.getName());
+        }
+
+        List<ParsedUsesPermissionImpl> extraUsesPerms = pkg.getPackageParsingHooks().addUsesPermissions();
+
+        if (extraUsesPerms != null) {
+            for (ParsedUsesPermission p : extraUsesPerms) {
+                String name = p.getName();
+                if (!usesPerms.add(name)) {
+                    Slog.w(TAG, "PackageParsingHooks.addUsesPermissions() " +
+                            "tried to add duplicate uses-permission " + name
+                            + " to pkg " + pkg.getPackageName());
+                    continue;
+                }
+                pkg.addUsesPermission(p);
+            }
+        }
+
         convertCompatPermissions(pkg);
 
         convertSplitPermissions(pkg);
@@ -1320,7 +1371,9 @@ public class ParsingPackageUtils {
         }
         ParsedPermission permission = result.getResult();
         if (permission != null) {
-            pkg.addPermission(permission);
+            if (!pkg.getPackageParsingHooks().shouldSkipPermissionDefinition(permission)) {
+                pkg.addPermission(permission);
+            }
         }
         return input.success(pkg);
     }
@@ -1475,7 +1528,10 @@ public class ParsingPackageUtils {
             }
 
             if (!found) {
-                pkg.addUsesPermission(new ParsedUsesPermissionImpl(name, usesPermissionFlags));
+                var p = new ParsedUsesPermissionImpl(name, usesPermissionFlags);
+                if (!pkg.getPackageParsingHooks().shouldSkipUsesPermission(p)) {
+                    pkg.addUsesPermission(p);
+                }
             }
             return success;
         } finally {
@@ -2293,6 +2349,15 @@ public class ParsingPackageUtils {
         if (hasReceiverOrder) {
             pkg.sortReceivers();
         }
+
+        List<ParsedService> extraServices = pkg.getPackageParsingHooks().addServices(pkg);
+        if (extraServices != null) {
+            for (var s : extraServices) {
+                hasServiceOrder |= (s.getOrder() != 0);
+                pkg.addService(s);
+            }
+        }
+
         if (hasServiceOrder) {
             pkg.sortServices();
         }
@@ -2390,6 +2455,14 @@ public class ParsingPackageUtils {
 
        // CHECKSTYLE:on
         //@formatter:on
+
+        var hooks = pkg.getPackageParsingHooks();
+        int enabledOverride = hooks.overrideDefaultPackageEnabledState();
+        if (enabledOverride == PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
+            pkg.setEnabled(false);
+        } else if (enabledOverride == PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {
+            pkg.setEnabled(true);
+        }
     }
 
     /**
