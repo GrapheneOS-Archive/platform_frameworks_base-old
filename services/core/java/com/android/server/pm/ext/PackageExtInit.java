01ecbeaa606e5dd4dd7c63606e0c62c972446129
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-27 12:10:37 +0200

    add PackageId definitions

--- services/core/java/com/android/server/pm/ext/PackageExtInit.java
+++ services/core/java/com/android/server/pm/ext/PackageExtInit.java
@@ -64,8 +64,34 @@ public class PackageExtInit {
 
     private int getPackageId() {
         return switch (pkg.getPackageName()) {
+            case GSF_NAME -> validate(GSF, 30L, mainGmsCerts());
+            case GMS_CORE_NAME -> validate(GMS_CORE, 21_00_00_000L, mainGmsCerts());
+            case PLAY_STORE_NAME -> validate(PLAY_STORE, 0L, mainGmsCerts());
+            case G_SEARCH_APP_NAME -> validate(G_SEARCH_APP, 0L, mainGmsCerts());
+            case EUICC_SUPPORT_PIXEL_NAME -> validateSystemPkg(EUICC_SUPPORT_PIXEL);
+            case G_EUICC_LPA_NAME -> validateSystemPkg(G_EUICC_LPA);
+            case G_CARRIER_SETTINGS_NAME -> validate(G_CARRIER_SETTINGS, 37L,
+                    "c00409b6524658c2e8eb48975a5952959ea3707dd57bc50fd74d6249262f0e82");
+            case G_CAMERA_NAME -> validate(G_CAMERA, 65820000L,
+                    "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83",
+                    "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00");
+            case PIXEL_CAMERA_SERVICES_NAME -> validate(PIXEL_CAMERA_SERVICES, 124000L,
+                    "226bb0439d6baeaa5a397c586e7031d8addfaec73c65be212f4a5dbfbf621b92");
+            case ANDROID_AUTO_NAME -> validate(ANDROID_AUTO, 11_0_635014L,
+                    "1ca8dcc0bed3cbd872d2cb791200c0292ca9975768a82d676b8b424fb65b5295");
 
             default -> PackageId.UNKNOWN;
+        };
+    }
+
+    private static String[] mainGmsCerts() {
+        return new String[] {
+                // "bd32" SHA256withRSA issued in March 2020
+                "7ce83c1b71f3d572fed04c8d40c5cb10ff75e6d87d9df6fbd53f0468c2905053",
+                // "38d1" MD5withRSA issued in August 2008
+                "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83",
+                // "58e1" MD5withRSA issued in April 2008
+                "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00",
         };
     }
 
