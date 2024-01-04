92e278a91fad05695fb0d35c872b521023bb47e6
Alex Buynytskyy <alexbuy@google.com>
2024-01-04 00:18:16 +0000

    Stop marking apps as privileged if they are not signed properly.
    Fixes: 311374917
    Test: atest android.content.pm.cts.PackageManagerTest
    (cherry picked from commit 3ee5dfdcba047051ce81dca0696d6ddfeafe2d98)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e336c7d7c9ceb49d934bbef0d27ecef344ad80ed)
    Merged-In: I5b5b81cf43b06837a22c8dfd170a112106dd64c1
    Change-Id: I5b5b81cf43b06837a22c8dfd170a112106dd64c1

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -4411,7 +4411,9 @@ final class InstallPackageHelper {
 
     private void assertPackageWithSharedUserIdIsPrivileged(AndroidPackage pkg)
             throws PackageManagerException {
-        if (!pkg.isPrivileged() && (pkg.getSharedUserId() != null)) {
+        if (!pkg.isPrivileged()
+                && (pkg.getSharedUserId() != null)
+                && !pkg.isLeavingSharedUid()) {
             SharedUserSetting sharedUserSetting = null;
             try {
                 sharedUserSetting = mPm.mSettings.getSharedUserLPw(pkg.getSharedUserId(),
@@ -4445,7 +4447,8 @@ final class InstallPackageHelper {
         if (((scanFlags & SCAN_AS_PRIVILEGED) == 0)
                 && !pkg.isPrivileged()
                 && (pkg.getSharedUserId() != null)
-                && !skipVendorPrivilegeScan) {
+                && !skipVendorPrivilegeScan
+                && !pkg.isLeavingSharedUid()) {
             SharedUserSetting sharedUserSetting = null;
             synchronized (mPm.mLock) {
                 try {
