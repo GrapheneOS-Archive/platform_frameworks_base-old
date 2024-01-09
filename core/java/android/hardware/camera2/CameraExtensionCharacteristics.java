7a1379690d3f6fa924f14c1ca1b37dfb858e0178
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-09 11:38:41 +0200

    Revert "allow "Pixel Camera Services" app to provide vendor camera extensions"
    These changes are no longer needed now that Pixel Camera Services is shipped with the OS.

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -47,8 +47,6 @@ import android.util.Pair;
 import android.util.Range;
 import android.util.Size;
 
-import com.android.internal.util.PixelCameraServicesUtils;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
@@ -256,10 +254,6 @@ public final class CameraExtensionCharacteristics {
         }
 
         private static boolean validateVendorCameraExtensionsPackage(Context ctx, String pkgName) {
-            if (PixelCameraServicesUtils.validatePackage(ctx, pkgName)) {
-                return true;
-            }
-
             try {
                 ApplicationInfo ai = ctx.getPackageManager().getApplicationInfo(pkgName, 0);
                 return ai.isSystemApp();
