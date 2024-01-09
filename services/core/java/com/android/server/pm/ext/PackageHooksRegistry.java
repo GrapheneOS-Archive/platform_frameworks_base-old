91e83747960783833ab0f10b33d2d95d0138772d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-09 11:38:03 +0200

    add config for Pixel Camera Services

--- services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
+++ services/core/java/com/android/server/pm/ext/PackageHooksRegistry.java
@@ -14,6 +14,7 @@ public class PackageHooksRegistry {
         return switch (pkgName) {
             case PackageId.EUICC_SUPPORT_PIXEL_NAME -> new EuiccSupportPixelHooks.ParsingHooks();
             case PackageId.G_EUICC_LPA_NAME -> new EuiccGoogleHooks.ParsingHooks();
+            case PackageId.PIXEL_CAMERA_SERVICES_NAME -> new PixelCameraServicesHooks.ParsingHooks();
             default -> PackageParsingHooks.DEFAULT;
         };
     }
@@ -23,6 +24,7 @@ public class PackageHooksRegistry {
             case PackageId.EUICC_SUPPORT_PIXEL -> new EuiccSupportPixelHooks();
             case PackageId.G_CARRIER_SETTINGS -> new GCarrierSettingsHooks();
             case PackageId.G_EUICC_LPA -> new EuiccGoogleHooks();
+            case PackageId.PIXEL_CAMERA_SERVICES -> new PixelCameraServicesHooks();
             default -> PackageHooks.DEFAULT;
         };
     }
