442c9999e221db556214fe97b1ee606f32acb75b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-09 11:38:41 +0200

    Revert "allow "Pixel Camera Services" app to provide vendor camera extensions"
    These changes are no longer needed now that Pixel Camera Services is shipped with the OS.

--- services/core/java/com/android/server/pm/AppsFilterImpl.java
+++ services/core/java/com/android/server/pm/AppsFilterImpl.java
@@ -31,7 +31,6 @@ import static com.android.internal.util.FrameworkStatsLog.PACKAGE_MANAGER_APPS_F
 import static com.android.internal.util.FrameworkStatsLog.PACKAGE_MANAGER_APPS_FILTER_CACHE_UPDATE_REPORTED__EVENT_TYPE__PACKAGE_ADDED;
 import static com.android.internal.util.FrameworkStatsLog.PACKAGE_MANAGER_APPS_FILTER_CACHE_UPDATE_REPORTED__EVENT_TYPE__PACKAGE_DELETED;
 import static com.android.internal.util.FrameworkStatsLog.PACKAGE_MANAGER_APPS_FILTER_CACHE_UPDATE_REPORTED__EVENT_TYPE__PACKAGE_REPLACED;
-import static com.android.server.ext.PackageManagerUtils.validatePackage;
 import static com.android.server.pm.AppsFilterUtils.canQueryAsInstaller;
 import static com.android.server.pm.AppsFilterUtils.canQueryAsUpdateOwner;
 import static com.android.server.pm.AppsFilterUtils.canQueryViaComponents;
@@ -62,7 +61,6 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.FrameworkStatsLog;
-import com.android.internal.util.PixelCameraServicesUtils;
 import com.android.server.FgThread;
 import com.android.server.compat.CompatChange;
 import com.android.server.om.OverlayReferenceMapper;
@@ -584,16 +582,12 @@ public final class AppsFilterImpl extends AppsFilterLocked implements Watchable,
 
         final boolean isGmsApp = GmsCompat.isEnabledFor(newPkg.ext().getPackageId(), newPkgSetting.isPrivileged());
 
-        final boolean isPixelCameraServices = validatePackage(newPkg,
-                PixelCameraServicesUtils.PACKAGE_SPEC);
-
         final boolean newIsForceQueryable;
         synchronized (mForceQueryableLock) {
             newIsForceQueryable = mForceQueryable.contains(newPkgSetting.getAppId())
                             /* shared user that is already force queryable */
                             || newPkgSetting.isForceQueryableOverride() /* adb override */
                             || isGmsApp
-                            || isPixelCameraServices
                             || (newPkgSetting.isSystem() && (mSystemAppsQueryable
                             || newPkg.isForceQueryable()
                             || ArrayUtils.contains(mForceQueryableByDevicePackageNames,
