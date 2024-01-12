6f8ab95e0a433f0d0a5b71d15ccf480fbaf45b98
Daniel Micay <daniel.micay@grapheneos.org>
2024-01-11 21:47:11 -0500

    reduce default auto-reboot timer to 18 hours

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -41,8 +41,8 @@ public class ExtSettings {
 
     public static final IntSetting AUTO_REBOOT_TIMEOUT = new IntSetting(
             Setting.Scope.GLOBAL, "settings_reboot_after_timeout",
-            // default value: 3 days
-            (int) TimeUnit.DAYS.toMillis(3));
+            // default value: 18 hours
+            (int) TimeUnit.HOURS.toMillis(18));
 
     public static final BoolSetting SCREENSHOT_TIMESTAMP_EXIF = new BoolSetting(
             Setting.Scope.PER_USER, "screenshot_timestamp_exif", false);
