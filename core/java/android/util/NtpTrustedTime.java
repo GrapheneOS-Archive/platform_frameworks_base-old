f4958b10b5823796103dfd894ac7dcaeaf5f8e31
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-23 14:21:17 +0200

    use HTTPS X-Time header for time updates instead of NTP
    Based on 57448ae65ee86fd218bd411947588b848658e5e8 and d17960e03d9b641dbed7ff330f77480a82d97faf
    
    Co-authored-by: Renlord <me@renlord.com>
    Co-authored-by: Daniel Micay <daniel.micay@grapheneos.org>

--- core/java/android/util/NtpTrustedTime.java
+++ core/java/android/util/NtpTrustedTime.java
@@ -23,6 +23,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.net.HttpsTimeClient;
 import android.net.Network;
 import android.net.NetworkInfo;
 import android.net.SntpClient;
@@ -38,6 +39,7 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -225,6 +227,10 @@ public abstract class NtpTrustedTime implements TrustedTime {
     @Nullable
     private URI mLastSuccessfulNtpServerUri;
 
+    @GuardedBy("this")
+    @Nullable
+    private URL mLastSuccessfulHttpsTimeUrl;
+
     // Declared volatile and accessed outside synchronized blocks to avoid blocking reads during
     // forceRefresh().
     private volatile TimeResult mTimeResult;
@@ -253,7 +259,7 @@ public abstract class NtpTrustedTime implements TrustedTime {
 
     /** Forces a refresh using the default network. */
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.R, trackingBug = 170729553)
-    public boolean forceRefresh() {
+    public final boolean forceRefresh() {
         synchronized (this) {
             Network network = getDefaultNetwork();
             if (network == null) {
@@ -266,7 +272,7 @@ public abstract class NtpTrustedTime implements TrustedTime {
     }
 
     /** Forces a refresh using the specified network. */
-    public boolean forceRefresh(@NonNull Network network) {
+    public final boolean forceRefresh(@NonNull Network network) {
         Objects.requireNonNull(network);
 
         synchronized (this) {
@@ -281,6 +287,24 @@ public abstract class NtpTrustedTime implements TrustedTime {
         if (!isNetworkConnected(network)) {
             if (LOGD) Log.d(TAG, "forceRefreshLocked: network=" + network + " is not connected");
             return false;
+        }
+
+        final String networkTimeMode = "https";
+
+        if ("https".equals(networkTimeMode)) {
+            var client = new HttpsTimeClient(getHttpsTimeConfig(), network);
+            HttpsTimeClient.Result res = client.requestTime(mLastSuccessfulHttpsTimeUrl);
+            if (res == null) {
+                return false;
+            }
+
+            mTimeResult = res.timeResult;
+            mLastSuccessfulHttpsTimeUrl = res.url;
+            return true;
+        }
+
+        if (!"ntp".equals(networkTimeMode)) {
+            throw new IllegalStateException(networkTimeMode);
         }
 
         NtpConfig ntpConfig = getNtpConfig();
@@ -355,6 +379,10 @@ public abstract class NtpTrustedTime implements TrustedTime {
             return mNtpConfigForTests;
         }
         return getNtpConfigInternal();
+    }
+
+    private HttpsTimeClient.Config getHttpsTimeConfig() {
+        return HttpsTimeClient.Config.getDefault(getContext());
     }
 
     /**
@@ -586,6 +614,8 @@ public abstract class NtpTrustedTime implements TrustedTime {
     /** Prints debug information. */
     public void dump(PrintWriter pw) {
         synchronized (this) {
+            pw.println("getHttpsTimeConfig()=" + getHttpsTimeConfig());
+            pw.println("mLastSuccessfulHttpsTimeUrl=" + mLastSuccessfulHttpsTimeUrl);
             pw.println("getNtpConfig()=" + getNtpConfig());
             pw.println("mNtpConfigForTests=" + mNtpConfigForTests);
             pw.println("mLastSuccessfulNtpServerUri=" + mLastSuccessfulNtpServerUri);
@@ -725,5 +755,14 @@ public abstract class NtpTrustedTime implements TrustedTime {
             }
             return (int) longValue;
         }
+
+        @NonNull
+        @Override
+        protected Context getContext() {
+            return mContext;
+        }
     }
+
+    @NonNull
+    protected abstract Context getContext();
 }
