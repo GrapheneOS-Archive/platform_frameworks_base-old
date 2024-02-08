32cbf46121c8b63e81b233e7685e41d331e288ff
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-02-08 11:29:31 +0200

    gmscompat: bugfix: always apply PhenotypeFlag overrides
    PhenotypeFlag overrides weren't applied when query of the phenotype database returned a null cursor,
    which happens when all flags in a given namespace use the default values.

--- core/java/android/content/ContentResolver.java
+++ core/java/android/content/ContentResolver.java
@@ -1247,6 +1247,10 @@ public abstract class ContentResolver implements ContentInterface {
                         queryArgs, remoteCancellationSignal);
             }
             if (qCursor == null) {
+                if (GmsCompat.isEnabled()) {
+                    return GmsHooks.maybeModifyQueryResult(uri, projection, queryArgs, null);
+                }
+
                 return null;
             }
 
