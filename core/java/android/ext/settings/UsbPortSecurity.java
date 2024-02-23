9a152a3c2bd1cbf52ffae0a5a1bf6e76e073a8ee
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-02-23 18:58:03 +0200

    add setting for USB port security state

---
+++ core/java/android/ext/settings/UsbPortSecurity.java
@@ -0,0 +1,16 @@
+package android.ext.settings;
+
+/** @hide */
+public class UsbPortSecurity {
+    public static final int MODE_DISABLED = 0;
+    public static final int MODE_CHARGING_ONLY = 1;
+    // doesn't apply to connections that were made before locking
+    public static final int MODE_CHARGING_ONLY_WHEN_LOCKED = 2;
+    // doesn't apply to connections that were made before locking or first unlock
+    public static final int MODE_CHARGING_ONLY_WHEN_LOCKED_AFU = 3;
+    public static final int MODE_ENABLED = 4;
+
+    // keep in sync with USB HAL implementations that check this sysprop during init
+    public static final IntSysProperty MODE_SETTING = new IntSysProperty(
+            "persist.security.usb_mode", MODE_ENABLED);
+}
