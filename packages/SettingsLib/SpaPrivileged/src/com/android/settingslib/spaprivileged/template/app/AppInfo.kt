73a178b29bd379f95a31ceccc3d9285497385640
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 12:40:48 +0200

    SettingsLib: show more APK details on App info screen

--- packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
+++ packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -44,6 +45,8 @@ import com.android.settingslib.spa.widget.ui.SettingsBody
 import com.android.settingslib.spa.widget.ui.SettingsTitle
 import com.android.settingslib.spaprivileged.R
 import com.android.settingslib.spaprivileged.model.app.rememberAppRepository
+import java.text.DateFormat
+import java.util.Date
 
 class AppInfoProvider(private val packageInfo: PackageInfo) {
     @Composable
@@ -89,17 +92,7 @@ class AppInfoProvider(private val packageInfo: PackageInfo) {
     @Composable
     fun FooterAppVersion(showPackageName: Boolean = rememberIsDevelopmentSettingsEnabled()) {
         val context = LocalContext.current
-        val footer = remember(packageInfo, showPackageName) {
-            val list = mutableListOf<String>()
-            packageInfo.versionNameBidiWrapped?.let {
-                list += context.getString(R.string.version_text, it)
-            }
-            if (showPackageName) {
-                list += packageInfo.packageName
-            }
-            list.joinToString(separator = System.lineSeparator())
-        }
-        if (footer.isBlank()) return
+        val footer = getFooterText()
         HorizontalDivider()
         Column(modifier = Modifier.padding(SettingsDimension.itemPadding)) {
             CopyableBody(footer)
@@ -118,6 +111,58 @@ class AppInfoProvider(private val packageInfo: PackageInfo) {
         /** Wrapped the version name, so its directionality still keep same when RTL. */
         val PackageInfo.versionNameBidiWrapped: String?
             get() = BidiFormatter.getInstance().unicodeWrap(versionName)
+    }
+
+    @Composable
+    private fun getFooterText(): String {
+        val ctx = LocalContext.current
+        val pi = packageInfo
+
+        val dateFormat = remember { android.text.format.DateFormat.getMediumDateFormat(ctx) }
+        val timeFormat = remember { android.text.format.DateFormat.getTimeFormat(ctx) }
+
+        fun formatDate(unixTs: Long, dateFormat: DateFormat, timeFormat: DateFormat): String {
+            val d = Date(unixTs)
+            return dateFormat.format(d) + "; " + timeFormat.format(d)
+        }
+
+        val appInfo = pi.applicationInfo
+
+        val lines = mutableListOf<String>()
+        pi.versionNameBidiWrapped?.let {
+            lines.add(stringResource(R.string.version_text, it))
+            lines.add("")
+        }
+        lines.add(pi.packageName)
+        lines.add("versionCode ${pi.getLongVersionCode()}")
+        lines.add("")
+        if (appInfo != null) {
+            lines.add("targetSdk ${appInfo.targetSdkVersion}")
+            lines.add("minSdk ${appInfo.minSdkVersion}")
+        }
+
+        // some system apps report being installed in January 2009, skip showing install time for them
+        val minTime = 1_240_000_000_000 // April 2009
+
+        var addedBlankLineBeforeTime = false
+
+        if (pi.firstInstallTime > minTime) {
+            lines.add("")
+            addedBlankLineBeforeTime = true
+            val s = formatDate(pi.firstInstallTime, dateFormat, timeFormat)
+            lines.add(stringResource(R.string.app_info_install_time, s))
+        }
+
+        if (pi.lastUpdateTime != pi.firstInstallTime) {
+            if (!addedBlankLineBeforeTime) {
+                lines.add("")
+                addedBlankLineBeforeTime = true
+            }
+            val s = formatDate(pi.lastUpdateTime, dateFormat, timeFormat)
+            lines.add(stringResource(R.string.app_info_update_time, s))
+        }
+
+        return lines.joinToString(separator = System.lineSeparator())
     }
 }
 
