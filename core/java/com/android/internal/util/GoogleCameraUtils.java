1a42a31aeeac14ab0fa8a9afef53c230613c752e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-11 13:58:50 +0200

    fixup! support denying special access to hardware accelerators by Google apps
    Adjust to seinfo override infrastructure changes in the previous commit.

--- core/java/com/android/internal/util/GoogleCameraUtils.java
+++ core/java/com/android/internal/util/GoogleCameraUtils.java
@@ -2,17 +2,12 @@ package com.android.internal.util;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.ext.PackageId;
 
 import com.android.internal.R;
 
 public class GoogleCameraUtils {
-    public static final String PACKAGE_NAME = "com.google.android.GoogleCamera";
-
-    public static final PackageSpec PACKAGE_SPEC = new PackageSpec(
-        PACKAGE_NAME, 65820000L, new String[] {
-            "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83",
-            "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00",
-    });
+    public static final String PACKAGE_NAME = PackageId.G_CAMERA_NAME;
 
     public static boolean isCustomSeInfoNeededForAccessToAccelerators(Context ctx) {
         Resources res = ctx.getResources();
