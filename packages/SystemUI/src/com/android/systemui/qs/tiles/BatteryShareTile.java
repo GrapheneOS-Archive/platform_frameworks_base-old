c55ac42dcc332e0000413443e04f5c2719be7bf0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-05-31 21:32:42 +0300

    fixup! SystemUI: add Battery share & Battery share quick tiles
    Fix build breakage of SystemUITests.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryShareTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryShareTile.java
@@ -15,7 +15,6 @@ import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.systemui.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
@@ -26,6 +25,7 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.res.R;
 
 import javax.inject.Inject;
 import vendor.google.wireless_charger.ReverseWirelessCharger;
